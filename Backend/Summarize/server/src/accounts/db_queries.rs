0649979531754f46c54098166fab4c791df3834d
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2024-04-10 20:39:43 +0100

    Update db_queries.rs

--- Backend/Summarize/server/src/accounts/db_queries.rs
+++ Backend/Summarize/server/src/accounts/db_queries.rs
@@ -25,7 +25,7 @@ pub async fn get_user_from_email_in_pg_users_table(pool: &Pool<Postgres>, email:
     return Ok(user)
 }
 
-pub fn set_token_user_in_redis(token: &str, user: &User, expiry_in_seconds: Option<i64>) {
+pub fn set_token_user_in_redis(token: &str, user: &User, expiry_in_seconds: &Option<i64>) {
 }
 
 pub fn get_user_from_token_in_redistoken: &str) -> Result<User, Error> {
