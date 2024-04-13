01440f523aed15f21ff4b588d4878bc71141aa39
1twalters01 <1twalters01@gmail.com>
2024-04-13 11:26:57 +0100

    switch to meson

--- Frontend/linux/src/main.c
+++ Frontend/linux/src/main.c
@@ -1,26 +1,20 @@
-#include <gtk-4.0/gtk/gtk.h>
+#include <gtk/gtk.h>
 
-static void print_hello(GtkWidget *widget, gpointer data) {
-    g_print("Hello, world!\n");
-}
-
-static void activate(GtkApplication *app, gpointer user_data) {
+static void activate(GtkApplication* app, gpointer user_data) {
     GtkWidget *window;
-    GtkWidget *button;
+    GtkWidget *label;
 
     window = gtk_application_window_new(app);
+    label = gtk_label_new("Hello world");
+
     gtk_window_set_title(GTK_WINDOW(window), "Summarize");
-    // gtk_window_maximize(GTK_WINDOW(window));
-    gtk_window_set_default_size (GTK_WINDOW (window), 500, 200);
+    gtk_window_set_default_size(GTK_WINDOW (window), 1700, 900);
 
-    button = gtk_button_new_with_label("Hello, world!");
-    g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
-    gtk_window_set_child(GTK_WINDOW(window), button);
-
+    gtk_window_set_child(GTK_WINDOW(window), label);
     gtk_window_present(GTK_WINDOW(window));
 }
 
-int main (int argc, char *argv[]) {
+int main(int argc, char **argv) {
     GtkApplication *app;
     int status;
 
