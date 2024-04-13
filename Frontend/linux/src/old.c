9a893ac792c3fe3f196c912e7e8c49c0e634b216
1twalters01 <1twalters01@gmail.com>
2024-04-13 16:09:52 +0100

    structure ideation

--- Frontend/linux/src/old.c
+++ Frontend/linux/src/old.c
@@ -1,49 +1,38 @@
 #include <gtk/gtk.h>
 
-typedef struct {
-    GtkWidget *window;
-    GtkWidget *current_widget;
-} WindowData;
-
+// Declare the widgets
 static void activate(GtkApplication *app, gpointer data);
 void widget1(GtkWidget *window, gpointer data);
 void widget2(GtkWidget *window, gpointer data);
 
+// Widget 1 - a button saying next window
 void widget1(GtkWidget *window, gpointer data) {
-    WindowData *window_data;
     GtkWidget *button1;
 
-    window_data = (WindowData *)data;
     button1 = gtk_button_new_with_label("next window");
-    g_signal_connect(button1, "clicked", G_CALLBACK(widget2), window_data);
-    gtk_window_set_child(GTK_WINDOW(window_data->window), button1);
+    g_signal_connect(button1, "clicked", G_CALLBACK(widget2), window);
+    gtk_window_set_child(GTK_WINDOW(window), button1);
 }
 
+// Widget 2 - a button saying previous window
 void widget2(GtkWidget *window, gpointer data) {
-    WindowData *window_data;
     GtkWidget *button2;
 
-    window_data = (WindowData *)data;
     button2 = gtk_button_new_with_label("previous window");
-    g_signal_connect(button2, "clicked", G_CALLBACK(widget1), window_data);
-    gtk_window_set_child(GTK_WINDOW(window_data->window), button2);
+    g_signal_connect(button2, "clicked", G_CALLBACK(widget1), window);
+    gtk_window_set_child(GTK_WINDOW(window), button2);
 }
+
 
 static void activate(GtkApplication *app, gpointer data) {
     GtkWidget *window;
-    WindowData *window_data;
-
+    
     window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "Summarize");
     gtk_window_maximize(GTK_WINDOW(window));
 
-    window_data = g_malloc(sizeof(WindowData));
-    window_data->window = window;
-
-    // Initially, add widget1 to the window
-    widget1(window, window_data);
-
-    gtk_widget_show(window);
+    widget1(window, data);
+    gtk_window_present(GTK_WINDOW(window));
 }
 
 int main(int argc, char **argv) {
@@ -57,3 +46,4 @@ int main(int argc, char **argv) {
 
     return status;
 }
+
