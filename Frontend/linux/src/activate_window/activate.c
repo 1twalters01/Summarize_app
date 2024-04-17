5f828d43ce8f34e78e05030e1eddedd9c93e83ec
1twalters01 <1twalters01@gmail.com>
2024-04-17 23:47:54 +0100

    save and retreive entry data to sqlite3 db (very dirty method)

--- Frontend/linux/src/activate_window/activate.c
+++ Frontend/linux/src/activate_window/activate.c
@@ -7,6 +7,7 @@
 static void instantiate_db() {
     sqlite3 *db;
     int rc;
+    sqlite3_stmt *stmt;
 
 
     rc = sqlite3_open("data/example.db", &db);
@@ -14,15 +15,26 @@ static void instantiate_db() {
         fprintf(stderr, "Error opening database: %s\n", sqlite3_errmsg(db));
     }
 
-    const char *sql = "CREATE TABLE IF NOT EXISTS EntryData (id INTEGER PRIMARY KEY, text TEXT);";
+    const char *sql = "CREATE TABLE IF NOT EXISTS EntryData (id INTEGER PRIMARY KEY, name TEXT, text TEXT);";
     const char *err = "Error reading sql";
     rc = sqlite3_exec(db, sql, NULL, 0, (char**) &err);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", err);
+        sqlite3_free((void *) err);
+    } else {
+        fprintf(stdout, "Table created successfully\n");
+    }
 
 
 
 
-    const char *check = "SELECT name FROM sqlite_master WHERE type='table'";
-    sqlite3_stmt *stmt;
+
+
+
+
+
+    // const char *check = "SELECT name FROM sqlite_master WHERE type='table'";
+    const char *check = "SELECT * FROM EntryData";
     rc = sqlite3_prepare_v2(db, check, -1, &stmt, NULL);
     if (rc != SQLITE_OK) {
         fprintf(stderr, "SQL error: %s\n", sqlite3_errmsg(db));
--- Frontend/linux/src/widgets/widget_test_2/widget_2.c
+++ Frontend/linux/src/widgets/widget_test_2/widget_2.c
@@ -7,31 +7,146 @@
 #include "widgets/widget_test_1_and_3/widget_1.h"
 #include "widgets/widget_test_1_and_3/widget_3.h"
 
-static const char *get_entry_data_from_db(void) {
+static char *get_entry_data_from_db(void) {
     sqlite3 *db;
     int rc;
-    sqlite3_stmt *stmt;
+    sqlite3_stmt *stmt1;
+    sqlite3_stmt *stmt2;
+    char *text = "";
+    char *result = "";
     
-    const char *query = "SELECT text FROM EntryData WHERE id=1";
     
-    rc = sqlite3_prepare_v2(db, check, -1, &stmt, NULL);
+    // text = NULL;
+    rc = sqlite3_open("data/example.db", &db);
     if (rc != SQLITE_OK) {
-        fprintf(stderr, "SQL error: %s\n", sqlite3_errmsg(db));
+        fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
+        return text;
+    }
+
+    const char *query = "SELECT * FROM EntryData WHERE name = ?";
+
+    rc = sqlite3_prepare_v2(db, query, -1, &stmt1, NULL);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Failed to prepare statement: %s\n", sqlite3_errmsg(db));
         sqlite3_close(db);
+        return text;
     }
 
-    if (sqlite3_step(stmt) == SQLITE_ROW) {
+    char *name = "example";
+    sqlite3_bind_text(stmt1, 1, name, -1, SQLITE_STATIC);
+
+    rc = sqlite3_step(stmt1);
+
+    if (rc == SQLITE_ROW) {
         // Need to change unsigned char * to char * some how
-        const unsigned char *text = sqlite3_column_text(stmt, 0);
-        printf("%s\n", text);
-    }
-    sqlite3_close(db);
+        text = (char *) sqlite3_column_text(stmt1, 2);
+        result = (char *) malloc(strlen(text) + 1);
+        if (result == NULL) {
+            fprintf(stderr, "Memory allocation failed\n");
+            return "";
+        }
+        strcpy(result, text);
+        printf("working test: %s\n", text);
+        printf("working result: %s\n", result);
+        strcpy(text, result);
+        printf("working test: %s\n", text);
+        printf("working result: %s\n", result);
+    } else if (rc == SQLITE_DONE) {
+        printf("No rows found with the given ID.\n");
 
-    return text;
+
+
+
+        const char *text = "test";
+        const char *insert_sql_insert = "INSERT INTO EntryData (name, text) VALUES (?, ?)";
+
+        rc = sqlite3_prepare_v2(db, insert_sql_insert, -1, &stmt2, 0);
+            
+        if (rc != SQLITE_OK) {
+            fprintf(stderr, "SQL error: %s\n", sqlite3_errmsg(db));
+        } 
+
+        sqlite3_bind_text(stmt2, 1, name, -1, SQLITE_STATIC);
+        sqlite3_bind_text(stmt2, 2, text, -1, SQLITE_STATIC);
+
+        rc = sqlite3_step(stmt2);
+        if (rc != SQLITE_DONE) {
+            fprintf(stderr, "SQL err: %s\n", sqlite3_errmsg(db));
+        }
+
+        sqlite3_finalize(stmt2);
+    } else {
+        fprintf(stderr, "Error retrieving data: %s\n", sqlite3_errmsg(db));
+    }
+
+    printf("text test: %s\n", text);
+    printf("text result: %s\n", result);
+
+    sqlite3_finalize(stmt1);
+    printf("text test: %s\n", text);
+    printf("text result: %s\n", result);
+    sqlite3_close(db);
+    
+    printf("text test: %s\n", text);
+    printf("text result: %s\n", result);
+
+    return (char *) result;
 }
 
 static void save_entry_data_to_db(GtkEditable *entry, gpointer data) {
     // perform sql query
+    sqlite3 *db;
+    int rc;
+    sqlite3_stmt *stmt;
+    const gchar *text = gtk_editable_get_text(GTK_EDITABLE(data));
+
+
+    printf("key has been pressed\n");
+
+
+    rc = sqlite3_open("data/example.db", &db);
+
+    const char *update_sql_insert = "UPDATE EntryData set text = (?) WHERE id = 1";
+    rc = sqlite3_prepare_v2(db, update_sql_insert, -1, &stmt, 0);
+            
+    if (rc == SQLITE_OK) {
+        sqlite3_bind_text(stmt, 1, text, -1, SQLITE_STATIC);
+        rc = sqlite3_step(stmt);
+
+        if (rc == SQLITE_DONE) {
+            sqlite3_finalize(stmt);
+            sqlite3_close(db);
+            return;
+        } else {
+            fprintf(stderr, "SQL error: %s\n", sqlite3_errmsg(db));
+        }
+
+    } else { 
+        fprintf(stderr, "SQL error: %s\n", sqlite3_errmsg(db)); 
+    }
+
+
+
+
+    const char *insert_sql_insert = "INSERT INTO EntryData (text) VALUES (?)";
+    rc = sqlite3_prepare_v2(db, insert_sql_insert, -1, &stmt, 0);
+            
+    if (rc == SQLITE_OK) {
+        sqlite3_bind_text(stmt, 1, text, -1, SQLITE_STATIC);
+        rc = sqlite3_step(stmt);
+
+        if (rc == SQLITE_DONE) {
+            sqlite3_finalize(stmt);
+        } else {
+            fprintf(stderr, "SQL err: %s\n", sqlite3_errmsg(db));
+        }
+
+    } else {
+        fprintf(stderr, "SQL error: %s\n", sqlite3_errmsg(db));
+    }
+    
+    sqlite3_close(db);
+    return;
 }
 
 static const char *widget_2_button_1_lua_content(void) {
@@ -62,6 +177,7 @@ void widget_2(GtkWidget *widget, gpointer data) {
     GtkWidget *button1;
     GtkWidget *button2;
     GtkWidget *entry;
+    GtkEntryBuffer *buffer;
     GtkWidget *submit;
 
     window_data = (GtkWidget *)data;
@@ -74,10 +190,16 @@ void widget_2(GtkWidget *widget, gpointer data) {
     g_signal_connect(button2, "clicked", G_CALLBACK(widget_3), window_data);
 
     entry = gtk_entry_new();
+    buffer = gtk_entry_buffer_new(NULL, 0);
+
+    char *buffer_text = get_entry_data_from_db();
+    printf("result is this: %s\n", buffer_text);
+    gtk_entry_set_buffer(GTK_ENTRY(entry), buffer);
+    gtk_entry_buffer_set_text(buffer, buffer_text, -1);
     const char *entry_text = "Enter text here:";
     gtk_entry_set_placeholder_text(GTK_ENTRY(entry), entry_text);
-    gtk_entry_set_editable(GTK_ENTRY(entry), get_entry_data_from_db());
     g_signal_connect(entry, "activate", G_CALLBACK(entry_submitted), entry);
+    g_signal_connect(entry, "changed", G_CALLBACK(save_entry_data_to_db), entry);
 
     submit = gtk_button_new_with_label("Submit");
     g_signal_connect(submit, "clicked", G_CALLBACK(entry_submitted), entry);
