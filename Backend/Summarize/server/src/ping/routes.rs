0c7fded90b2a78be479b1b0ed9ba1d164e72b5d9
1twalters01 <1twalters01@gmail.com>
2024-04-27 01:21:27 +0100

    get and post tests for ping on server

--- Backend/Summarize/server/src/ping/routes.rs
+++ Backend/Summarize/server/src/ping/routes.rs
@@ -98,3 +98,67 @@ async fn ping_post_not_auth(data: Json<String>) -> Result<impl Responder> {
         .json(dual_message)
     )
 }
+
+#[cfg(test)]
+mod tests {
+    use actix_web::{test, App};
+    use serde_json::json;
+
+    use super::*;
+
+    #[actix_web::test]
+    async fn test_ping_get_any_auth() {
+        let mut app = test::init_service(App::new().service(ping_get_any_auth)).await;
+
+        let mut request = test::TestRequest::get().uri("/ping/any_auth").to_request();
+        let mut response = test::call_service(&mut app, request).await;
+        assert!(response.status().is_success());
+
+        request = test::TestRequest::get().uri("/ping/any_auth").to_request();
+        response = test::call_service(&mut app, request).await;
+        let res: serde_json::Value = test::read_body_json(response).await;
+        assert_eq!(res, json!({"message": "Ping any authorization level from server"}));
+    }
+
+    // #[actix_web::test]
+    // fn test_ping_get_only_auth() {
+    // }
+    //
+    // #[actix_web::test]
+    // fn test_ping_get_not_auth() {
+    // }
+
+    #[actix_web::test]
+    async fn test_ping_post_any_auth() {
+        let mut app = test::init_service(App::new().service(ping_post_any_auth)).await;
+
+        let data_text: String = String::from("Ping from test");
+        let data: Message = Message { message: data_text.clone() };
+
+        let mut request = test::TestRequest::post()
+            .set_json(&data)
+            .uri("/ping/any_auth")
+            .to_request();
+        let mut response = test::call_service(&mut app, request).await;
+        assert!(response.status().is_success());
+
+        request = test::TestRequest::post()
+            .set_json(&data)
+            .uri("/ping/any_auth")
+            .to_request();
+        response = test::call_service(&mut app, request).await;
+        let res: serde_json::Value = test::read_body_json(response).await;
+        assert_eq!(res, json!({
+            "message_1": "Ping any authorization level from server",
+            "message_2": format!("Request data: {}", data_text)
+        }));
+    }
+
+    // #[actix_web::test]
+    // fn test_ping_post_only_auth() {
+    // }
+    //
+    // #[actix_web::test]
+    // fn test_ping_post_not_auth() {
+    // }
+}
