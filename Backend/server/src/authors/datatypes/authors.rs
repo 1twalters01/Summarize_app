a191ed4a812995b7f976ac3d3a1f698825062d24
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2024-05-24 00:39:10 +0100

    Create authors.rs

---
+++ Backend/server/src/authors/datatypes/authors.rs
@@ -0,0 +1,75 @@
+use users::user::User;
+use uuid::Uuid;
+
+pub struct Author {
+    uuid: Uuid,
+    first_name: Option<String>,
+    last_name: Option<String>,
+    bio: Option<String>,
+    social_media: Vec<SocialMedia>,
+    user: Option<User>,
+    average_rating: f32,
+}
+
+impl Author {
+    pub fn new(first_name: Option<String>, last_name: Option<String>) -> Author {
+        Self {
+            uuid: Uuid::new_v4(),
+            first_name,
+            last_name,
+            bio: None
+            social_media: Vec::new(),
+            user: None,
+            average_rating: 0.0,
+            media: Vec::new(),
+        }
+    }
+
+    pub fn get_uuid(&self) -> Uuid {
+        self.uuid.to_owned()
+    }
+
+    pub fn get_full_name(&self) -> String {
+        let first_name = self.get_first_name();
+        let last_name = self.get_last_name();
+
+        return format!("{} {}", first_name.to_owned(), last_name.to_owned());
+    }
+
+    pub fn get_first_name(&self) -> String {
+        let first_name = match self.first_name.clone() {
+            Some(first_name) => first_name,
+            None => String::new(),
+        };
+        return first_name;
+    }
+
+    pub fn get_last_name(&self) -> String {
+        let last_name = match self.last_name.clone() {
+            Some(last_name) => last_name,
+            None => String::new(),
+        };
+        return last_name;
+    }
+
+    pub fn get_bio(&self) -> String {
+        let bio = match self.bio.clone() {
+            Some(bio) => bio,
+            None => String::new(),
+        };
+        return bio;
+    }
+
+    pub fn is_user(&self) -> bool {
+        self.user.is_some()
+    }
+
+    pub fn get_user(&self) -> Option<String> {
+        let id: Option<String> = match self.user {
+            Ok(res) => res.get_id(&self).unwrap(),
+            None => None,
+        };
+
+        return id;
+    }
+}
