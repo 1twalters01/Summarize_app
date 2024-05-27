80f0c4e4e80758c9dcd75592e84f0f2ae6b2eb55
1twalters01 <1twalters01@gmail.com>
2024-05-27 21:22:38 +0100

    some book structure done

---
+++ Backend/server/src/later/books/datatypes/books.rs
@@ -0,0 +1,59 @@
+use chronos::time;
+
+pub struct Book {
+    information: Information,
+    details: Details,
+    synopsis: Synopsis,
+    
+    related_works: Related,
+}
+
+pub struct Information {
+    title: String,
+    subtitle: String,
+    authors: Vec<Author>,
+    secondary_authors: Vec<Author>,
+    publication_date: time,
+    publisher: Publisher,
+}
+
+pub struct Details {
+    isbn: ISBN,
+    editions: Vec<String>,
+    formats: Vec<Format>,
+    page_count: u64,
+    original_language: String,
+    cover_image: String, //link
+}
+
+pub enum Format {
+    Hardcover,
+    Paperback,
+    ebook,
+    audiobook,
+}
+
+pub struct Synopsis {
+    summary: String,
+    genre: Genre,
+    themes: Vec<String>,
+}
+
+pub struct Related {
+    prequels: Vec<Book>,
+    sequels: Vec<Book>,
+    other: Vec<Book>,
+    adaptations: Vec<Adaptation>,
+}
+
+pub struct Adaptation {
+    type: AdaptationType, // Films, tv shows, plays, etc.
+    link: String,
+}
+
+pub enum AdaptationType {
+    Film,
+    TVShow,
+    Play,
+}
+
