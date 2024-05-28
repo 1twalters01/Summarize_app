a47d5ec0ba2cf666a7c4e772e2ee1493fc9e446b
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2024-05-28 20:18:58 +0100

    Rough verify captcha route

--- Backend/server/src/accounts/routes/captcha.rs
+++ Backend/server/src/accounts/routes/captcha.rs
@@ -1,1 +1,33 @@
+use actix_web::{post, web, HttpResponse, Responder, Result};
+use serde::Deserialize;
 
+#[derive(Deserialize)]
+struct CaptchaResponse {
+    response: String,
+}
+
+#[post("/verify_captcha")]
+async fn verify_captcha(data: web::Json<CaptchaResponse>) -> impl Result<Responder> {
+    // Retrieve the solution from the session or database
+    let con = create_redis_client_connection();
+    let solution: User = match get_captcha_solution_from_token_in_redis(con, &token) {
+        // if error return error
+        Err(err) => {
+            let error: AccountError = AccountError {
+                is_error: true,
+                error_message: Some(err),
+            };
+            res_body.account_error = error;
+            return Ok(HttpResponse::UnprocessableEntity()
+                .content_type("application/json; charset=utf-8")
+                .json(res_body));
+        }
+        Ok(solution) => solution,
+    };
+
+    if data.response == solution {
+        return Ok(HttpResponse::Ok());
+    } else {
+        return Ok(HttpResponse::Unauthorized());
+    }
+}
