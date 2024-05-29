532838fea3e62dd7e3ff1e23b9b761ad4978d3db
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2024-05-29 22:11:24 +0100

    High level plan for user profiles file

---
+++ Backend/server/src/subscriptions/user_management/user_profiles.rs
@@ -0,0 +1,15 @@
+
+
+// Get user profile - subscription status, current payment provider
+
+// get subscription history
+// start date, end date, amount, subscription plan, transaction id,
+
+// Get detailed specific subscription info - request info from payment processor, rate limiting:
+//     transaction id,
+//     start date, end date, amount,
+//     subscription period, subscription plan, payment method,
+//     billing information, payment status, renewal details, tax information,
+//     notes, customer support references
+
+// Generate or get invoice for chosen payment record
