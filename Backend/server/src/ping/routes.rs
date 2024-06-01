c198ec0de1d13c972a4216c64c6676b878a3e0d1
1twalters01 <1twalters01@gmail.com>
2024-06-01 17:47:37 +0100

    separation of conserns (urls of ping)

--- Backend/server/src/ping/routes.rs
+++ Backend/server/src/ping/routes.rs
@@ -2,8 +2,7 @@ use crate::{accounts::auth::Claims, ping::datatypes::{DualMessage, Message}};
 use actix_web::{get, post, web::Json, HttpMessage, HttpRequest, HttpResponse, Responder, Result};
 
 // get (any auth)
-#[get("ping/any_auth")]
-async fn ping_get_any_auth() -> Result<impl Responder> {
+pub async fn ping_get_any_auth() -> Result<impl Responder> {
     let message: Message = Message {
         message: String::from("Ping any authorization level from server"),
     };
@@ -14,8 +13,7 @@ async fn ping_get_any_auth() -> Result<impl Responder> {
 }
 
 // post (any auth)
-#[post("ping/any_auth")]
-async fn ping_post_any_auth(data: Json<Message>) -> Result<impl Responder> {
+pub async fn ping_post_any_auth(data: Json<Message>) -> Result<impl Responder> {
     println!("data: {:#?}", data);
     let req_data: String = data.into_inner().message;
 
@@ -34,14 +32,13 @@ async fn ping_post_any_auth(data: Json<Message>) -> Result<impl Responder> {
 }
 
 // get (only auth)
-#[get("ping/only_auth")]
-async fn ping_get_only_auth(req: HttpRequest) -> Result<impl Responder> {
+pub async fn ping_get_only_auth(req: HttpRequest) -> Result<impl Responder> {
     let message: Message = Message {
         message: String::from("Ping only authorized level from server"),
     };
 
-    let claims: Claims = req.extensions().get::<Claims>().unwrap().clone();
-    println!("Claims: {:#?}", claims);
+    // let claims: Claims = req.extensions().get::<Claims>().unwrap().clone();
+    // println!("Claims: {:#?}", claims);
 
     return Ok(HttpResponse::Ok()
         .content_type("application/json; charset=utf-8")
@@ -49,8 +46,7 @@ async fn ping_get_only_auth(req: HttpRequest) -> Result<impl Responder> {
 }
 
 // post (only auth)
-#[post("ping/only_auth")]
-async fn ping_post_only_auth(data: Json<Message>) -> Result<impl Responder> {
+pub async fn ping_post_only_auth(data: Json<Message>) -> Result<impl Responder> {
     let req_data: String = data.into_inner().message;
 
     let message_1: String = String::from("Ping only authorized level from server");
@@ -66,8 +62,7 @@ async fn ping_post_only_auth(data: Json<Message>) -> Result<impl Responder> {
 }
 
 // get (only not auth)
-#[get("ping/not_auth")]
-async fn ping_get_not_auth() -> Result<impl Responder> {
+pub async fn ping_get_not_auth() -> Result<impl Responder> {
     let message: Message = Message {
         message: String::from("Ping only not authorized level from server"),
     };
@@ -78,8 +73,7 @@ async fn ping_get_not_auth() -> Result<impl Responder> {
 }
 
 // post (only not auth)
-#[post("ping/not_auth")]
-async fn ping_post_not_auth(data: Json<Message>) -> Result<impl Responder> {
+pub async fn ping_post_not_auth(data: Json<Message>) -> Result<impl Responder> {
     let req_data: String = data.into_inner().message;
 
     let message_1: String = String::from("Ping only not authorized level from server");
@@ -97,15 +91,18 @@ async fn ping_post_not_auth(data: Json<Message>) -> Result<impl Responder> {
 
 #[cfg(test)]
 mod tests {
-    use actix_web::{test, App};
+    use actix_web::{test, web, App};
     use dotenv::dotenv;
     use serde_json::json;
 
     use crate::{accounts::{auth::generate_access_token, datatypes::users::User}, middleware, ping::routes::*};
 
     #[actix_web::test]
-    async fn test_ping_get_any_auth() {
-        let mut app = test::init_service(App::new().service(ping_get_any_auth)).await;
+    async fn test_ping_get_any_auth_not_authenticated() {
+        let mut app = test::init_service(App::new().service(
+                web::scope("/ping")
+                .route("/any_auth", web::get().to(ping_get_any_auth))
+        )).await;
 
         let mut request = test::TestRequest::get().uri("/ping/any_auth").to_request();
         let mut response = test::call_service(&mut app, request).await;
@@ -121,8 +118,45 @@ mod tests {
     }
 
     #[actix_web::test]
-    async fn test_ping_post_any_auth() {
-        let mut app = test::init_service(App::new().service(ping_post_any_auth)).await;
+    async fn test_ping_get_any_auth_authenticated() {
+        let mut app = test::init_service(App::new().service(
+                web::scope("/ping")
+                .route("/any_auth", web::get().to(ping_get_any_auth))
+        )).await;
+
+        let username = String::from("test123");
+        let email = String::from("test123@gmail.com");
+        let password = String::from("i23oj3rfw");
+
+        let user: User = User::new(username, email, password).unwrap();
+        let token = generate_access_token(&user); 
+        let auth_token = String::from("Bearer ") + &token;  
+
+        let mut request = test::TestRequest::get()
+            .uri("/ping/any_auth")
+            .insert_header(("Authorization", auth_token.clone()))
+            .to_request();
+        let mut response = test::call_service(&mut app, request).await;
+        assert!(response.status().is_success());
+
+        request = test::TestRequest::get()
+            .uri("/ping/any_auth")
+            .insert_header(("Authorization", auth_token))
+            .to_request();
+        response = test::call_service(&mut app, request).await;
+        let res: serde_json::Value = test::read_body_json(response).await;
+        assert_eq!(
+            res,
+            json!({"message": "Ping any authorization level from server"})
+        );
+    }
+
+    #[actix_web::test]
+    async fn test_ping_post_any_auth_not_authenticated() {
+        let mut app = test::init_service(App::new().service(
+                web::scope("/ping")
+                .route("/any_auth", web::post().to(ping_post_any_auth))
+        )).await;
 
         let data_text: String = String::from("Ping from test");
         let data: Message = Message {
@@ -152,11 +186,61 @@ mod tests {
     }
 
     #[actix_web::test]
+    async fn test_ping_post_any_auth_authenticated() {
+        let mut app = test::init_service(App::new().service(
+                web::scope("/ping")
+                .route("/any_auth", web::post().to(ping_post_any_auth))
+        )).await;
+
+        let username = String::from("test123");
+        let email = String::from("test123@gmail.com");
+        let password = String::from("i23oj3rfw");
+
+        let user: User = User::new(username, email, password).unwrap();
+        let token = generate_access_token(&user); 
+        let auth_token = String::from("Bearer ") + &token;  
+
+
+        let data_text: String = String::from("Ping from test");
+        let data: Message = Message {
+            message: data_text.clone(),
+        };
+
+        let mut request = test::TestRequest::post()
+            .set_json(&data)
+            .uri("/ping/any_auth")
+            .insert_header(("Authorization", auth_token.clone()))
+            .to_request();
+        let mut response = test::call_service(&mut app, request).await;
+        assert!(response.status().is_success());
+
+        request = test::TestRequest::post()
+            .set_json(&data)
+            .uri("/ping/any_auth")
+            .insert_header(("Authorization", auth_token.clone()))
+            .to_request();
+        response = test::call_service(&mut app, request).await;
+        let res: serde_json::Value = test::read_body_json(response).await;
+        assert_eq!(
+            res,
+            json!({
+                "message_1": "Ping any authorization level from server",
+                "message_2": format!("Request data: {}", data_text)
+            })
+        );
+    }
+
+    #[actix_web::test]
     async fn test_ping_get_only_auth_not_authenticated() {
-        let mut app = test::init_service(App::new().wrap(middleware::authentication::is_authenticated::IsAuthenticated).service(ping_get_only_auth)).await;
+        let mut app = test::init_service(App::new().service(
+                web::scope("/ping")
+                .wrap(middleware::authentication::is_authenticated::IsAuthenticated)
+                .route("/only_auth", web::get().to(ping_get_only_auth))
+        )).await;
 
         let request = test::TestRequest::get().uri("/ping/only_auth").to_request();
         let response = test::call_service(&mut app, request).await;
+        println!("{}", response.status());
         assert!(response.status().is_client_error());
     }
 
@@ -164,7 +248,11 @@ mod tests {
     async fn test_ping_get_only_auth_authenticated() {
         dotenv().ok();
 
-        let mut app = test::init_service(App::new().wrap(middleware::authentication::is_authenticated::IsAuthenticated).service(ping_get_only_auth)).await;
+        let mut app = test::init_service(App::new().service(
+                web::scope("/ping")
+                .wrap(middleware::authentication::is_authenticated::IsAuthenticated)
+                .route("/only_auth", web::get().to(ping_get_only_auth))
+        )).await;
 
         let username = String::from("test123");
         let email = String::from("test123@gmail.com");
@@ -196,7 +284,11 @@ mod tests {
 
     #[actix_web::test]
     async fn test_ping_post_only_auth_not_authenticated() {
-        let mut app = test::init_service(App::new().wrap(middleware::authentication::is_authenticated::IsAuthenticated).service(ping_post_only_auth)).await;
+        let mut app = test::init_service(App::new().service(
+                web::scope("/ping")
+                .wrap(middleware::authentication::is_authenticated::IsAuthenticated)
+                .route("/only_auth", web::post().to(ping_post_only_auth))
+        )).await;
 
         let data_text: String = String::from("Ping from test");
         let data: Message = Message {
@@ -213,7 +305,11 @@ mod tests {
 
     #[actix_web::test]
     async fn test_ping_post_only_auth_authenticated() {
-        let mut app = test::init_service(App::new().wrap(middleware::authentication::is_authenticated::IsAuthenticated).service(ping_post_only_auth)).await;
+        let mut app = test::init_service(App::new().service(
+                web::scope("/ping")
+                .wrap(middleware::authentication::is_authenticated::IsAuthenticated)
+                .route("/only_auth", web::post().to(ping_post_only_auth))
+        )).await;
 
         let username = String::from("test123");
         let email = String::from("test123@gmail.com");
@@ -256,7 +352,11 @@ mod tests {
 
     #[actix_web::test]
     async fn test_ping_get_not_auth_not_authenticated() {
-        let mut app = test::init_service(App::new().wrap(middleware::authentication::not_authenticated::NotAuthenticated).service(ping_get_not_auth)).await;
+        let mut app = test::init_service(App::new().service(
+                web::scope("/ping")
+                .wrap(middleware::authentication::not_authenticated::NotAuthenticated)
+                .route("/not_auth", web::get().to(ping_get_not_auth))
+        )).await;
 
         let mut request = test::TestRequest::get().uri("/ping/not_auth").to_request();
         let mut response = test::call_service(&mut app, request).await;
@@ -273,7 +373,11 @@ mod tests {
 
     #[actix_web::test]
     async fn test_ping_get_not_auth_authenticated() {
-        let mut app = test::init_service(App::new().wrap(middleware::authentication::not_authenticated::NotAuthenticated).service(ping_get_not_auth)).await;
+        let mut app = test::init_service(App::new().service(
+                web::scope("/ping")
+                .wrap(middleware::authentication::not_authenticated::NotAuthenticated)
+                .route("/not_auth", web::get().to(ping_get_not_auth))
+        )).await;
 
         let username = String::from("test123");
         let email = String::from("test123@gmail.com");
@@ -294,7 +398,11 @@ mod tests {
 
     #[actix_web::test]
     async fn test_ping_post_not_auth_not_authenticated() {
-        let mut app = test::init_service(App::new().wrap(middleware::authentication::not_authenticated::NotAuthenticated).service(ping_post_not_auth)).await;
+         let mut app = test::init_service(App::new().service(
+                web::scope("/ping")
+                .wrap(middleware::authentication::not_authenticated::NotAuthenticated)
+                .route("/not_auth", web::post().to(ping_post_not_auth))
+        )).await;
 
         let data_text: String = String::from("Ping from test");
         let data: Message = Message {
@@ -326,7 +434,11 @@ mod tests {
 
     #[actix_web::test]
     async fn test_ping_post_not_auth_authenticated() {
-        let mut app = test::init_service(App::new().wrap(middleware::authentication::not_authenticated::NotAuthenticated).service(ping_post_not_auth)).await;
+         let mut app = test::init_service(App::new().service(
+                web::scope("/ping")
+                .wrap(middleware::authentication::not_authenticated::NotAuthenticated)
+                .route("/not_auth", web::post().to(ping_post_not_auth))
+        )).await;
 
         let username = String::from("test123");
         let email = String::from("test123@gmail.com");
@@ -347,7 +459,6 @@ mod tests {
             .insert_header(("Authorization", auth_token.clone()))
             .to_request();
         let response = test::call_service(&mut app, request).await;
-        println!("status: {}", response.status());
         assert!(response.status().is_client_error());
     }
 }
--- Backend/server/src/ping/urls.rs
+++ Backend/server/src/ping/urls.rs
@@ -1,11 +1,14 @@
 use crate::ping::routes;
-use actix_web::web::ServiceConfig;
+use actix_web::web::{self, ServiceConfig};
 
 pub fn config(cfg: &mut ServiceConfig) {
-    cfg.service(routes::ping_get_any_auth)
-        .service(routes::ping_get_only_auth)
-        .service(routes::ping_get_not_auth)
-        .service(routes::ping_post_any_auth)
-        .service(routes::ping_post_only_auth)
-        .service(routes::ping_post_not_auth);
+    cfg.service(
+        web::scope("/ping")
+        .route("/any_auth", web::get().to(routes::ping_get_any_auth))
+        .route("/any_auth", web::post().to(routes::ping_post_any_auth))
+        .route("/only_auth", web::get().to(routes::ping_get_only_auth))
+        .route("/only_auth", web::post().to(routes::ping_post_only_auth))
+        .route("/not_auth", web::get().to(routes::ping_get_not_auth))
+        .route("/not_auth", web::post().to(routes::ping_post_not_auth))
+    );
 }
