f1b26d636fb53bef62fde6dacf0361d6ddcc9071
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2024-06-16 21:58:13 +0100

    Initial Summary idea

---
+++ Backend/server/src/later/summaries/datatypes/summary.rs
@@ -0,0 +1,35 @@
+/* - Summmary
+  * Summary Title
+  * Summary Author (User)
+      * is_first_name_shown?
+      * is_last_name_shown?
+  * Book
+  * Counts
+      * Summary word count
+      * Summary character count
+      * Expected Time to Read
+      * Average Time to Read
+  * Downloads
+      * Total Downloads
+      * Unique User Downloads
+  * Sections
+      * Section Title
+      * Section Subtitle
+      * Associated Book Chapters
+      * Key Points
+      * Section Counts
+          * Section word count
+          * Section character count
+          * Section Expected Time to Read
+          * Section Average Time to Read
+          * Section number of paragraphs
+  * Views
+      * Times opened
+      * Read at least 30% of review
+      * Full read count
+  * Key Points
+  * Likes
+  * Follows
+  * is_public?
+  * is_complete?
+*/
