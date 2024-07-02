a3e3747538df2f2c3a702a2fb4128082cbd8aa80
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2024-07-02 20:45:43 +0100

    Update summary_item.jsx

--- Frontend/website/content/src/components/summary_item.jsx
+++ Frontend/website/content/src/components/summary_item.jsx
@@ -1,27 +1,32 @@
 /** @typedef {Object} SummaryData
+  * @param {string} summary_url - The url to the suummary
   * @param {string} picture_url - The url to the picture of the book
   * @param {Book} book - The book the summary summarizes
   * @param {string} author - The user that wrote the summary
+  * @param {string} author_url - The summary author's url
 */
-
 
 /** typedef {Object} Book
   * title {string} - The title of the book
+  * title_url {string} - The book's url
   * author {string} - The author of the book
+  * author_url {string} - The book author's url
 */
+
 
 /**
   * @param {SummaryData} book_data - The summary data required for a slide
 */
-const summary_item = (summary_data) => {
+const SummaryItem = (summary_data) => {
+  // Add loading state
   return (
     <div class="summary_item">
-      <img src={summary_data.picture_url} />
-      <p class="book_title">{summary_data.book.title}</p>
-      <p class="book_author">{summary_data.book.author}</p>
-      <p class="summary_author">{summary_data.author}</p>
+      <a href={summary_data.summary_url} ><img src={summary_data.picture_url} /></a>
+      <a href={summary_data.book.title_url} class="book_title">{summary_data.book.title}</a>
+      <a href={summary_data.book.author_url} class="book_author">{summary_data.book.author}</a>
+      <a href={summary_data.summary_author_url} class="summary_author">{summary_data.author}</a>
     </div>
   )
 }
 
-export default summary_item;
+export default SummaryItem;
