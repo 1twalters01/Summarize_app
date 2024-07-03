2d714d0b5ab2589b1f7c540010907dfdfec28ef5
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2024-07-03 22:39:46 +0100

    Create your_summaries.jsx

---
+++ Frontend/website/content/src/home/components/your_summaries.jsx
@@ -0,0 +1,18 @@
+// Placeholder things
+const YourSummaries = () => {
+  return (
+    <div class="your-summaries">
+      <div class="top">
+        <h3>Your Summaries</h3>
+
+        <h3><a href="/editor/all">View All</a></h3>
+      </div>
+
+      <div class="bottom">
+        <OwnSummaryArray own_summaary_data={own_summary_data_array}/>
+      </div>
+    <div />
+  )
+};
+
+export default YourSummaries;
