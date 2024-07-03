011cb1eb48441263e9a5f894c4c382fe12f3ec11
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2024-07-03 22:50:57 +0100

    Create Links.jsx

---
+++ Frontend/website/content/src/home/components/Links.jsx
@@ -0,0 +1,10 @@
+const Links = () => {
+  return (
+    <div>
+      <a href="">Write a summary</a>
+      <a href="">Search for summaries <img href="" /></a>
+    </div>
+  )
+}
+
+export default Links
