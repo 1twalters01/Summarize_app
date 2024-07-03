e74996dbb31c9da4eac0b1f06c887123d95faf81
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2024-07-03 23:00:16 +0100

    Create footer.jsx

---
+++ Frontend/website/content/src/home/components/footer.jsx
@@ -0,0 +1,38 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div class="home">
+        <a href="/home">
+          <img href="" />
+          <p class="">Home</p>
+        </a>
+      </div>
+      <div class="libraries">
+        <a href="/libraries">
+          <img href="" />
+          <p class="">Libraries</p>
+        </a>
+      </div>
+      <div class="">
+        <a href="">
+          <img href="" />
+          <p class=""></p>
+        </a>
+      </div>
+      <div class="settings">
+        <a href="/settings">
+          <img href="" />
+          <p class="">Settings</p>
+        </a>
+      </div>
+      <div class="editor">
+        <a href="/editor">
+          <img href="" />
+          <p class="">Write</p>
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default Footer;
