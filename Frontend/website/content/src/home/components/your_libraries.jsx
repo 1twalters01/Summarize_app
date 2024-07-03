9f1dc8f39f3ff51e08ed25c0605aae6b953dc26f
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2024-07-03 21:46:05 +0100

    Create your_libraries.jsx

---
+++ Frontend/website/content/src/home/components/your_libraries.jsx
@@ -0,0 +1,17 @@
+const YourLibraries = () => {
+  return (
+    <div class="your-libraries">
+      <div class="top">
+        <h3>Your Libraries</h3>
+
+        <h3><a href="/libraries">View All</a></h3>
+      </div>
+
+      <div class="bottom">
+        <LibraryArray library_data={library_data_array}/>
+      </div>
+    <div />
+  )
+};
+
+export default YourLibraries;
