44192a8df5e19fef1c66badeec533bf7734785b0
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2024-07-03 22:47:54 +0100

    Create recommended_shorts.jsx

---
+++ Frontend/website/content/src/home/components/recommended_shorts.jsx
@@ -0,0 +1,27 @@
+const RecommendedShorts = () => {
+  function filterByGenre(e) {
+    // Do a post request to a new summaries api and then update summary_data_array
+  }
+  
+  return (
+    <div class="recommended-shorts">
+      <div class="top">
+        <div class="left">
+          <h3>Recommended Shorts</h3>
+        </div>
+
+        <div class="right">
+          <For each={genres()}>{(genre) =>
+              <btn onClick={filterByGenre}>{genre}</btn>
+          }</For>
+        </div>
+      </div>
+
+      <div class="bottom">
+        <SummaryArray summary_data={summary_data_array} />
+      </div>
+    <div />
+  )
+};
+
+export default RecommendedShorts;
