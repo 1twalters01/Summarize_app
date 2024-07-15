b47be972572f6622282c9df9d9cfa016363b99ab
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2024-07-15 22:25:49 +0100

    Create Example.rs

---
+++ Backend/server/src/later/summaries/routes/Example.rs
@@ -0,0 +1,10 @@
+use actix_web::{web::Json, HttpRequest, HttpResponse, Responder, Result};
+
+pub async fn genres(data: Json<i64>) -> Result<impl Responder> {
+  
+}
+
+ 
+pub async fn summaries(data: Json<>) -> Result<impl Responder> {
+  
+}
