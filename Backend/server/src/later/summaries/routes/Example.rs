601b68d4d5a06cbe122567438d35312ca3464ad2
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2024-07-16 21:33:29 +0100

    Example genres function

--- Backend/server/src/later/summaries/routes/Example.rs
+++ Backend/server/src/later/summaries/routes/Example.rs
@@ -59,7 +59,26 @@ pub async fn get_favourite_genres(data: Json<FavouriteGenresRequestStruct>) -> R
 
 pub async fn get_favourite_genres_from_uuid(user_uuid: String, upper_limit: u8) {
 }
- 
+
+pub async fn get_example_genres() -> Result<impl Responder> {
+  let FavouriteGenresRequestSchema { number_of_genres } = data.into_inner();
+  let mut res_body: FavouriteGenresResponseSchema = FavouriteGenresResponseSchema::new();
+  
+  let request_limit = 15;
+  let upper_limit = cmp::min(request_limit, number_of_genres);
+
+  let genre_arr: [&str; 15] =
+    ["Fantasy", "Science fiction", "Dystopian Fiction", "Horror", "Mystery",
+     "History", "Engineering", "Thriller", "Graphic novel", "Philosophy",
+     "Self-help", "Psychology", "Autobiographies", "Design", "Business"];
+  let genre_vec:Vec<String> = genre_arr[0..upper_limit].iter().map(|s| String::from(*s)).collect();
+
+  res_body.genres = genre_vec;
+  return Ok(HttpResponse::Ok()
+      .content_type("application/json; charset=utf-8")
+      .json(res_body));
+}
+
 pub async fn summaries(data: Json<>) -> Result<impl Responder> {
   
 }
