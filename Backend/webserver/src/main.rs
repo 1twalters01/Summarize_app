5730c489e545bd4cda5e450c5c9d50fe88711610
1twalters01 <1twalters01@gmail.com>
2024-07-19 18:48:19 +0100

    folder restructuring

--- Backend/webserver/src/main.rs
+++ Backend/webserver/src/main.rs
@@ -1,16 +1,20 @@
 use actix_web::{web::{get, route, Bytes}, App, HttpResponse, HttpServer};
 use actix_files as files;
 use std::{fs, path::PathBuf};
+use dotenv::dotenv;
 mod utils;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
     HttpServer::new(|| {
         App::new()
+            .route("/favicon.ico", get().to(utils::routes::favicon_ico))
             .route("/main.js", get().to(utils::routes::main_js))
             .route("/", get().to(utils::routes::main_html))
             .route("{param:.*[^.bundle.js]}", get().to(utils::routes::main_html))
-            .service(files::Files::new("", "../content/dist/main/javascript"))
+            .service(files::Files::new("", "../../Frontend/website/dist/main/javascript"))
             .default_service(
                 route().to(|| async {
                     let path: PathBuf = "../content/dist/main/index.html".into();
--- Backend/webserver/src/utils/routes.rs
+++ Backend/webserver/src/utils/routes.rs
@@ -1,8 +1,9 @@
 use actix_web::{HttpResponse, Responder, Result, web::Bytes};
-use std::{fs, path::PathBuf};
+use std::{fs, path::PathBuf, env};
 
 pub async fn main_html() -> Result<impl Responder> {
-    let path: PathBuf = "../content/dist/main/index.html".into();
+    let website_dir: String = env::var("WEBSITE_DIR").unwrap();
+    let path: PathBuf = format!("{}/dist/main/index.html", website_dir).into();
     let data = Bytes::from(fs::read(&path).unwrap());
 
     Ok(HttpResponse::Ok()
@@ -10,13 +11,23 @@ pub async fn main_html() -> Result<impl Responder> {
         .body(data))
 }
 
+pub async fn favicon_ico() -> Result<impl Responder> {
+    let website_dir: String = env::var("WEBSITE_DIR").unwrap();
+    let path: PathBuf = format!("{}/dist/public/favicons/favicon.ico", website_dir).into();
+    let data = Bytes::from(fs::read(&path).unwrap());
+
+    Ok(HttpResponse::Ok()
+        .content_type("image/png; charset=UTF-8")
+        .body(data))
+}
+
 pub async fn main_js() -> Result<impl Responder> {
-    let path: PathBuf = "../content/dist/main/javascript/bundle.js".into();
+    let website_dir: String = env::var("WEBSITE_DIR").unwrap();
+    let path: PathBuf = format!("{}/dist/main/javascript/bundle.js", website_dir).into();
     let data = Bytes::from(fs::read(&path).unwrap());
 
     Ok(HttpResponse::Ok()
         .content_type("text/javascript; charset=UTF-8")
         .body(data))
 }
-
 
