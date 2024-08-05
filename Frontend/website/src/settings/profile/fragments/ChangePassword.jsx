071d59cfa3b84ea94922cb0615355feb362a6e0c
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2024-08-05 21:05:48 +0100

    Update ChangePassword.jsx

--- Frontend/website/src/settings/profile/fragments/ChangePassword.jsx
+++ Frontend/website/src/settings/profile/fragments/ChangePassword.jsx
@@ -1,7 +1,28 @@
+import { createSignal } from 'solid-js';
+
 const ChangePassword = () => {
+  const [password, setPassword] = createSignal('');
+  const [passwordConfirmation, setPasswordConfirmation] = createSignal('');
+  
   return (
     <>
       <h1>Change Password</h1>
+
+      <form onSubmit={(e) => handlePostPasswords(e, password(), passwordConfirmation(), navigate)}>
+        <input
+          type="password"
+          placeholder="password"
+          onInput={(e) => setPassword(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="password confirmation"
+          onInput={(e) => setPasswordConfirmation(e.target.value)}
+          required
+        />
+        <input type="submit" value="Submit" />
+      </form>
     </>
   );
 };
