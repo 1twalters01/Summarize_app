9e6e10c9ff85e698d65c0bcfd1c6ecba268df895
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2024-08-05 20:38:54 +0100

    Update ChangeName.jsx

--- Frontend/website/src/settings/profile/fragments/ChangeName.jsx
+++ Frontend/website/src/settings/profile/fragments/ChangeName.jsx
@@ -1,7 +1,25 @@
 const ChangeName = () => {
+  const [firstName, setFirstName] = createSignal('');
+  const [lastName, setLastName] = createSignal('');
+  
   return (
     <>
       <h1>Change Name</h1>
+
+      <form onSubmit={ (e) => handlePostName(e, password(), rememberMe(), setEmail) }>
+        <input
+          type="text"
+          placeholder="first name"
+          onInput={(e) => setFirstName(e.target.value)}
+          required
+        />
+        <input
+          type="checkbox"
+          placeholder="last name"
+          onInput={(e) => setLastName(e.target.checked)}
+        />
+        <input type="submit" value="Submit" />
+      </form>
     </>
   );
 };
