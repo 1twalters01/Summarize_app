a56f6f8d3ab16fdee91e32a492c487d81971e7b8
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2024-08-05 20:54:28 +0100

    Update ChangeUsername.jsx

--- Frontend/website/src/settings/profile/fragments/ChangeUsername.jsx
+++ Frontend/website/src/settings/profile/fragments/ChangeUsername.jsx
@@ -1,7 +1,19 @@
 const ChangeUsername = () => {
+  const [username, setUsername] = createSignal('');
+  
   return (
     <>
       <h1>Change Username</h1>
+
+      <form onSubmit={(e) => handlePostUsername(e, username())}>
+        <input
+          type="text"
+          placeholder="username"
+          onInput={(e) => setUsername(e.target.value)}
+          required
+        />
+        <input type="submit" value="Continue" />
+      </form>
     </>
   );
 };
