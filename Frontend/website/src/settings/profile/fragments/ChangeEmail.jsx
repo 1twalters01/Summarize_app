56989e52f0730c284a5702c2e478c9fd7e431489
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2024-08-05 20:29:53 +0100

    Update ChangeEmail.jsx

--- Frontend/website/src/settings/profile/fragments/ChangeEmail.jsx
+++ Frontend/website/src/settings/profile/fragments/ChangeEmail.jsx
@@ -1,7 +1,20 @@
 const ChangeEmail = () => {
+  const { email, setEmail } = useEmailContext();
+  
   return (
     <>
       <h1>Change Email</h1>
+
+      <form onSubmit={(e) => handlePostEmail(e, email(), props)}>
+        <input
+          type="email"
+          placeholder="email"
+          onInput={(e) => setEmail(e.target.value)}
+          value={email()}
+          required
+        />
+        <input type="submit" value="Continue" />
+      </form>
     </>
   );
 };
