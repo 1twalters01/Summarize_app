910a45191711a10b84639f080173d3ad357421d6
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2024-08-11 14:02:33 +0100

    Update password.rs

--- Backend/servers/accounts/src/settings/routes/profile/password.rs
+++ Backend/servers/accounts/src/settings/routes/profile/password.rs
@@ -1,146 +1,279 @@
 use crate::{
-    accounts::{
-        schema::auth::Claims,
-        datatypes::users::User,
-        queries::postgres::update_password_for_user_in_pg_users_table,
+    generated::protos::settings::profile::{
+        confirmation::{
+            response as confirmation_response,
+            Request as PasswordRequest,
+            Response as PasswordResponse,
+            Error as PasswordError,
+            Success as PasswordSuccess,
+        },
+        password::{
+            request::Request as MainRequest,
+            response::{
+                response,
+                Response as MainResponse,
+                Error as MainError
+            },
+        },
     },
-    settings::schema::{
-        ChangePasswordRequestStruct, ChangePasswordResponseStruct,
-        SettingsError,
+    accounts::{
+        datatypes::users::User,
+        queries::postgres::get_user_from_email_in_pg_users_table,
+        schema::auth::Claims,
     },
     utils::{
-        database_connections::create_pg_pool_connection,
-        validations::validate_password,
+        database_connections::{
+            create_pg_pool_connection,
+            create_redis_client_connection,
+            set_key_value_in_redis
+        },
+        tokens::generate_opaque_token_of_length,
+        validations::{validate_email, validate_password}
     },
 };
 use actix_web::HttpMessage;
 use actix_web::{post, web::Json, HttpRequest, HttpResponse, Responder, Result};
 use sqlx::{Pool, Postgres};
 
+#[derive(Serialize, Deserialize)]
+struct EmailTokenObject {
+    user_uuid: String,
+    password: String
+}
+
 #[post("change-password")]
 async fn change_password(
-    req_body: Json<ChangePasswordRequestStruct>,
+    req_body: ProtoBuf<MainRequest>,
     req: HttpRequest,
 ) -> Result<impl Responder> {
-    let ChangePasswordRequestStruct {
-        new_password,
-        new_password_confirmation,
-        password,
-    } = req_body.into_inner();
-    let mut res_body: ChangePasswordResponseStruct = ChangePasswordResponseStruct::new();
+    let MainRequest { new_password, new_password_confirmation} = req_body.0;
 
     // error if new_password != new_password_confirmation
     if new_password != new_password_confirmation {
-        let error: SettingsError = SettingsError {
-            is_error: true,
-            error_message: Some(String::from(
-                "confirmation is not the same as the new password",
-            )),
+        let response: MainResponse = MainResponse {
+            response_field: Some(response::ResponseField::Error(MainError::PasswordsDoNotMatch as i32)),
         };
-        res_body.settings_error = error;
-
         return Ok(HttpResponse::UnprocessableEntity()
-            .content_type("application/json; charset=utf-8")
-            .json(res_body));
+            .content_type("application/x-protobuf; charset=utf-8")
+            .protobuf(response));
     }
 
     // validate password
     let validated_password = validate_password(&password);
     if validated_password.is_err() {
-        let error: SettingsError = SettingsError {
-            is_error: true,
-            error_message: Some(validated_password.err().unwrap()),
+        let response: MainResponse = MainResponse {
+            response_field: Some(response::ResponseField::Error(MainError::InvalidCredentials as i32)),
         };
-        res_body.settings_error = error;
-
         return Ok(HttpResponse::UnprocessableEntity()
-            .content_type("application/json; charset=utf-8")
-            .json(res_body));
+            .content_type("application/x-protobuf; charset=utf-8")
+            .protobuf(response));
     }
 
-    // validate new password
-    let validated_new_password = validate_password(&new_password);
-    if validated_new_password.is_err() {
-        let error: SettingsError = SettingsError {
-            is_error: true,
-            error_message: Some(validated_new_password.err().unwrap()),
-        };
-        res_body.settings_error = error;
-
-        return Ok(HttpResponse::UnprocessableEntity()
-            .content_type("application/json; charset=utf-8")
-            .json(res_body));
-    }
-
-    // authenticate password
+    // Validate user
     let user_uuid: String = match req.extensions().get::<Claims>() {
         Some(claims) => claims.sub.clone(),
         None => {
-            res_body.settings_error = SettingsError {
-                is_error: true,
-                error_message: Some(String::from("error")),
+            let response: MainResponse = MainResponse {
+                response_field: Some(response::ResponseField::Error(MainError::InvalidCredentials as i32)),
             };
             return Ok(HttpResponse::InternalServerError()
-                .content_type("application/json; charset=utf-8")
-                .json(res_body));
+                .content_type("application/x-protobuf; charset=utf-8")
+                .protobuf(response));
         }
     };
-
     let user_result: Result<Option<User>, sqlx::Error> = User::from_uuid_str(&user_uuid).await;
-    let mut user: User = match user_result {
+    _ = match user_result {
         Err(_) => {
-            res_body.settings_error = SettingsError {
-                is_error: true,
-                error_message: Some(String::from("error")),
+            let response: MainResponse = MainResponse {
+                response_field: Some(response::ResponseField::Error(MainError::ServerError as i32)),
             };
             return Ok(HttpResponse::InternalServerError()
-                .content_type("application/json; charset=utf-8")
-                .json(res_body));
+                .content_type("application/x-protobuf; charset=utf-8")
+                .protobuf(response));
         }
         Ok(user) => match user {
             Some(user) => user,
             None => {
-                res_body.settings_error = SettingsError {
-                    is_error: true,
-                    error_message: Some(String::from("error")),
+                let response: MainResponse = MainResponse {
+                    response_field: Some(response::ResponseField::Error(MainError::InvalidCredentials as i32)),
                 };
                 return Ok(HttpResponse::InternalServerError()
-                    .content_type("application/json; charset=utf-8")
-                    .json(res_body));
+                    .content_type("application/x-protobuf; charset=utf-8")
+                    .protobuf(response));
             },
         },
     };
 
-    if user.check_password(&password).is_err() {
-        res_body.success = false;
-        res_body.settings_error = SettingsError {
-            is_error: true,
-            error_message: Some(String::from("incorrect password")),
+    // error if password has already been used
+    let pool = create_pg_pool_connection().await;
+    let user_result: Result<Option<User>, sqlx::Error> =
+        get_user_from_email_in_pg_users_table(&pool, &email).await;
+
+    let is_password_stored = (&user_result).as_ref().ok().is_some();
+    if is_password_stored == true {
+        let response: MainResponse = MainResponse {
+            response_field: Some(response::ResponseField::Error(MainError::PreviouslyUsedPassword as i32)),
         };
-        return Ok(HttpResponse::Unauthorized()
-            .content_type("application/json; charset=utf-8")
-            .json(res_body));
+
+        return Ok(HttpResponse::Conflict()
+            .content_type("application/x-protobuf; charset=utf-8")
+            .protobuf(response));
     }
 
-    // change password
-    user.set_password(password);
+    // hash password
+        
+    // Generate token
+    let token: String = generate_opaque_token_of_length(25);
+    let token_object: PasswordTokenObject = PasswordTokenObject{user_uuid, password};
+    let token_object_json = serde_json::to_string(&token_object).unwrap();
+
+    // Save key: token, value: {jwt, email} to redis
+    let expiry_in_seconds: Option<i64> = Some(300);
+    let con = create_redis_client_connection();
+    let set_redis_result = set_key_value_in_redis(con, &token, &token_object_json, &expiry_in_seconds);
+
+    // err handling
+    if set_redis_result.is_err() {
+        let response: MainResponse = MainResponse {
+            response_field: Some(response::ResponseField::Error(MainError::ServerError as i32)),
+        };
+        return Ok(HttpResponse::FailedDependency()
+            .content_type("application/x-protobuf; charset=utf-8")
+            .protobuf(response));
+    }
+
+    // return token
+    let response: MainResponse = MainResponse {
+        response_field: Some(response::ResponseField::RequiresPassword(true)),
+    };
+    return Ok(HttpResponse::Ok()
+        .content_type("application/x-protobuf; charset=utf-8")
+        .protobuf(response));
+}
+
+#[post("change-email")]
+async fn post_confirmation(
+    req_body: ProtoBuf<PasswordRequest>,
+    req: HttpRequest,
+) -> Result<impl Responder> {
+    let PasswordRequest { password } = req_body.0;
+    let login_email_token: String = req
+        .headers()
+        .get("Change-Password-Token")
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .to_string();
+
+    // validate password
+    let validated_password = validate_password(&password);
+    if validated_password.is_err() {
+        let response: PasswordResponse = PasswordResponse {
+            response_field: Some(confirmation_response::ResponseField::Error(PasswordError::InvalidCredentials as i32)),
+        };
+        return Ok(HttpResponse::UnprocessableEntity()
+            .content_type("application/x-protobuf; charset=utf-8")
+            .protobuf(response));
+    }
+    
+    // Validate user
+    let user_uuid: String = match req.extensions().get::<Claims>() {
+        Some(claims) => claims.sub.clone(),
+        None => {
+            let response: PasswordResponse = PasswordResponse {
+                response_field: Some(confirmation_response::ResponseField::Error(PasswordError::InvalidCredentials as i32)),
+            };
+            return Ok(HttpResponse::InternalServerError()
+                .content_type("application/x-protobuf; charset=utf-8")
+                .protobuf(response));
+        }
+    };
+    let user_result: Result<Option<User>, sqlx::Error> = User::from_uuid_str(&user_uuid).await;
+    let user: User = match user_result {
+        Err(_) => {
+            let response: PasswordResponse = PasswordResponse {
+                response_field: Some(confirmation_response::ResponseField::Error(PasswordError::ServerError as i32)),
+            };
+            return Ok(HttpResponse::InternalServerError()
+                .content_type("application/x-protobuf; charset=utf-8")
+                .protobuf(response));
+        }
+        Ok(user) => match user {
+            Some(user) => user,
+            None => {
+            let response: PasswordResponse = PasswordResponse {
+                response_field: Some(confirmation_response::ResponseField::Error(PasswordError::InvalidCredentials as i32)),
+            };
+            return Ok(HttpResponse::InternalServerError()
+                .content_type("application/x-protobuf; charset=utf-8")
+                .protobuf(response));
+            },
+        },
+    };
+    
+    // authenticate password
+    if user.check_password(&password).is_err() {
+        let response: PasswordResponse = PasswordResponse {
+            response_field: Some(confirmation_response::ResponseField::Error(PasswordError::IncorrectPassword as i32)),
+        };
+        return Ok(HttpResponse::Unauthorized()
+            .content_type("application/x-protobuf; charset=utf-8")
+            .protobuf(response));
+    };
+
+    // Get password hash from redis
+    let con = create_redis_client_connection();
+    let password_hash: String = match get_object_from_token_in_redis(con, &login_password_token) {
+            // if error return error
+            Err(err) => {
+                println!("err: {:#?}", err);
+                let response: PasswordResponse = PasswordResponse {
+                    response_field: Some(confirmation_response::ResponseField::Error(
+                        PasswordError::InvalidCredentials as i32,
+                    )),
+                };
+
+                return Ok(HttpResponse::UnprocessableEntity()
+                    .content_type("application/x-protobuf; charset=utf-8")
+                    .protobuf(response));
+            },
+            Ok(object) => match object.user_uuid == user_uuid {
+                true => {object.email},
+                false => {
+                    let response: PasswordResponse = PasswordResponse {
+                        response_field: Some(confirmation_response::ResponseField::Error(
+                            PasswordError::ServerError as i32,
+                        )),
+                    };
+
+                    return Ok(HttpResponse::UnprocessableEntity()
+                        .content_type("application/x-protobuf; charset=utf-8")
+                        .protobuf(response));
+                }
+            },
+        };
+
+    // change email
     let pool = create_pg_pool_connection().await;
     let update_result: Result<(), sqlx::Error> =
-        update_password_for_user_in_pg_users_table(&pool, &user).await;
+        update_password_for_user_in_pg_users_table(&pool, &email).await;
 
     // if sql update error then return an error
     if update_result.is_err() {
-        res_body.settings_error = SettingsError {
-            is_error: true,
-            error_message: Some(String::from("internal error")),
+        let response: PasswordResponse = PasswordResponse {
+            response_field: Some(confirmation_response::ResponseField::Error(PasswordError::ServerError as i32)),
         };
         return Ok(HttpResponse::FailedDependency()
-            .content_type("application/json; charset=utf-8")
-            .json(res_body));
+            .content_type("application/x-protobuf; charset=utf-8")
+            .protobuf(response));
     }
 
+    // return ok
+    let response: PasswordResponse = PasswordResponse {
+        response_field: Some(confirmation_response::ResponseField::Success(PasswordSuccess {  })),
+    };
     return Ok(HttpResponse::Ok()
-        .content_type("application/json; charset=utf-8")
-        .json(res_body));
-}
+        .content_type("application/x-protobuf; charset=utf-8")
+        .protobuf(response));
 
+}
