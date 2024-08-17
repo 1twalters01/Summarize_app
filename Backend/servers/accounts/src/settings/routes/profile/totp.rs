b4fba2116e540c933b8ebc57f5b3d042d678ea1e
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2024-08-17 08:39:46 +0100

    Create totp.rs

---
+++ Backend/servers/accounts/src/settings/routes/profile/totp.rs
@@ -0,0 +1,181 @@
+use crate::{
+    accounts::{
+        datatypes::users::User, queries::postgres::delete_user_from_uuid_in_pg_users_table,
+        schema::auth::Claims,
+    },
+    generated::protos::settings::profile::totp::{
+        response as password_response, Error as PasswordError, Request as PasswordRequest,
+        Response as PasswordResponse, Success as PasswordSuccess,
+    },
+    utils::{database_connections::create_pg_pool_connection, validations::validate_password},
+};
+use actix_protobuf::{ProtoBuf, ProtoBufResponseBuilder};
+use actix_web::{HttpMessage, HttpRequest, HttpResponse, Responder, Result};
+
+pub async fn post_totp(
+    req_body: ProtoBuf<PasswordRequest>,
+    req: HttpRequest,
+) -> Result<impl Responder> {
+    let PasswordRequest { password } = req_body.0;
+
+    // validate password
+    let validated_password = validate_password(&password);
+    if validated_password.is_err() {
+        let response: PasswordResponse = PasswordResponse {
+            response_field: Some(confirmation_response::ResponseField::Error(
+                PasswordError::InvalidCredentials as i32,
+            )),
+        };
+        return Ok(HttpResponse::UnprocessableEntity()
+            .content_type("application/x-protobuf; charset=utf-8")
+            .protobuf(response));
+    }
+
+    // Validate user
+    let user_uuid: String = match req.extensions().get::<Claims>() {
+        Some(claims) => claims.sub.clone(),
+        None => {
+            let response: PasswordResponse = PasswordResponse {
+                response_field: Some(confirmation_response::ResponseField::Error(
+                    PasswordError::InvalidCredentials as i32,
+                )),
+            };
+            return Ok(HttpResponse::InternalServerError()
+                .content_type("application/x-protobuf; charset=utf-8")
+                .protobuf(response));
+        }
+    };
+    let user_result: Result<Option<User>, sqlx::Error> = User::from_uuid_str(&user_uuid).await;
+    let user: User = match user_result {
+        Err(_) => {
+            let response: PasswordResponse = PasswordResponse {
+                response_field: Some(confirmation_response::ResponseField::Error(
+                    PasswordError::ServerError as i32,
+                )),
+            };
+            return Ok(HttpResponse::InternalServerError()
+                .content_type("application/x-protobuf; charset=utf-8")
+                .protobuf(response));
+        }
+        Ok(user) => match user {
+            Some(user) => user,
+            None => {
+                let response: PasswordResponse = PasswordResponse {
+                    response_field: Some(confirmation_response::ResponseField::Error(
+                        PasswordError::InvalidCredentials as i32,
+                    )),
+                };
+                return Ok(HttpResponse::InternalServerError()
+                    .content_type("application/x-protobuf; charset=utf-8")
+                    .protobuf(response));
+            }
+        },
+    };
+
+    // authenticate password
+    if user.check_password(&password).is_err() {
+        let response: PasswordResponse = PasswordResponse {
+            response_field: Some(confirmation_response::ResponseField::Error(
+                PasswordError::IncorrectPassword as i32,
+            )),
+        };
+        return Ok(HttpResponse::Unauthorized()
+            .content_type("application/x-protobuf; charset=utf-8")
+            .protobuf(response));
+    };
+
+    // Generate token
+    let token: String = generate_opaque_token_of_length(25);
+    let token_object: String = user_uuid;
+
+    // Save key: token, value: {jwt, email} to redis
+    let expiry_in_seconds: Option<i64> = Some(300);
+    let con = create_redis_client_connection();
+    let set_redis_result =
+        set_key_value_in_redis(con, &token, &token_object, &expiry_in_seconds);
+
+    // err handling
+    if set_redis_result.is_err() {
+        let response: PasswordResponse = PasswordResponse {
+            response_field: Some(password_response::ResponseField::Error(
+                PasswordError::ServerError as i32,
+            )),
+        };
+        return Ok(HttpResponse::FailedDependency()
+            .content_type("application/x-protobuf; charset=utf-8")
+            .protobuf(response));
+    }
+
+    // return ok
+    let response: PasswordResponse = PasswordResponse {
+        response_field: Some(confirmation_response::ResponseField::Success(
+            PasswordSuccess {},
+        )),
+    };
+    return Ok(HttpResponse::Ok()
+        .content_type("application/x-protobuf; charset=utf-8")
+        .protobuf(response));
+}
+
+pub async fn post_confirmation(
+    req_body: ProtoBuf<TotpRequest>,
+    req: HttpRequest,
+) -> Result<impl Responder> {
+    let PasswordRequest { digit1, digit2, digit3, digit4, digit5, digit6 } = req_body.0;
+    let login_email_token: String = req
+        .headers()
+        .get("Change-Totp-Token")
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .to_string();
+
+    // validate totp
+    // Validate user
+    let user_uuid: String = match req.extensions().get::<Claims>() {
+        Some(claims) => claims.sub.clone(),
+        None => {
+            let response: PasswordResponse = PasswordResponse {
+                response_field: Some(confirmation_response::ResponseField::Error(
+                    PasswordError::InvalidCredentials as i32,
+                )),
+            };
+            return Ok(HttpResponse::InternalServerError()
+                .content_type("application/x-protobuf; charset=utf-8")
+                .protobuf(response));
+        }
+    };
+    let user_result: Result<Option<User>, sqlx::Error> = User::from_uuid_str(&user_uuid).await;
+    let user: User = match user_result {
+        Err(_) => {
+            let response: PasswordResponse = PasswordResponse {
+                response_field: Some(confirmation_response::ResponseField::Error(
+                    PasswordError::ServerError as i32,
+                )),
+            };
+            return Ok(HttpResponse::InternalServerError()
+                .content_type("application/x-protobuf; charset=utf-8")
+                .protobuf(response));
+        }
+        Ok(user) => match user {
+            Some(user) => user,
+            None => {
+                let response: PasswordResponse = PasswordResponse {
+                    response_field: Some(confirmation_response::ResponseField::Error(
+                        PasswordError::InvalidCredentials as i32,
+                    )),
+                };
+                return Ok(HttpResponse::InternalServerError()
+                    .content_type("application/x-protobuf; charset=utf-8")
+                    .protobuf(response));
+            }
+        },
+    };
+
+    // authenticate totp
+    // get uuid from redis
+    // if saved_uuid != uuid then error
+    // get current totp status
+    // if totp = there then delete
+    // if totp = none then create totp key and save it / set totp to there
+}
