4fe5b0e0aec4bf49df09e3731e2fc62266514481
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2024-08-19 22:56:43 +0100

    Update theme.rs

--- Backend/servers/accounts/src/settings/routes/profile/theme.rs
+++ Backend/servers/accounts/src/settings/routes/profile/theme.rs
@@ -81,7 +81,7 @@ pub async fn post_language(
     // change theme
     let pool = create_pg_pool_connection().await;
     let update_result: Result<(), sqlx::Error> =
-        update_theme_for_user_in_pg_users_table(&pool, &user, &theme).await;
+        update_theme_for_user_in_pg_users_table(&pool, &user, &theme.unwrap()).await;
 
     // if sql update error then return an error
     if update_result.is_err() {
@@ -109,13 +109,68 @@ pub async fn post_language(
 pub async fn update_theme_for_user_in_pg_users_table(
     pool: &Pool<Postgres>,
     user: &User,
-    theme: &Option<RequestField>,
+    theme: &RequestField,
 ) -> Result<(), sqlx::Error> {
-    let user_update_query = sqlx::query("").bind("").execute(pool).await;
+    match theme {
+        RequestField::Custom(Custom{primary, secondary}) => {
+            // If theme is custom then set the fields and set the is_theme_preset to custom
+            if let Some(Colours { colour_1, colour_2, colour_3, colour_4, colour_5, colour_6 }) = primary {
+                let user_update_query = sqlx::query(
+                    "UPDATE users SET primary_1=($1), primary_2=($2), primary_3=($3), primary_4=($4), primary_5=($5), primary_6=($6) WHERE uuid=($7);"
+                )
+                    .bind(colour_1)
+                    .bind(colour_2)
+                    .bind(colour_3)
+                    .bind(colour_4)
+                    .bind(colour_5)
+                    .bind(colour_6)
+                    .bind(user_user.get_uuid())
+                    .execute(pool)
+                    .await;
+    
+                if let Err(err) = user_update_query {
+                    return Err(err);
+                } else {
+                    return Ok(());
+                }
+            }
 
-    if let Err(err) = user_update_query {
-        return Err(err);
-    } else {
-        return Ok(());
+            if let Some(Colours { colour_1, colour_2, colour_3, colour_4, colour_5, colour_6 }) = secondary {
+                let user_update_query = sqlx::query(
+                    "UPDATE users SET secondary_1=($1), secondary_2=($2), secondary_3=($3), secondary_4=($4), secondary_5=($5), secondary_6=($6) WHERE uuid=($7);"
+                )
+                    .bind(colour_1)
+                    .bind(colour_2)
+                    .bind(colour_3)
+                    .bind(colour_4)
+                    .bind(colour_5)
+                    .bind(colour_6)
+                    .bind(user_user.get_uuid())
+                    .execute(pool)
+                    .await;
+    
+                if let Err(err) = user_update_query {
+                    return Err(err);
+                } else {
+                    return Ok(());
+                }
+            }
+        },
+        RequestField::Presets(theme) => {
+            // If theme is a preset then set the preset field to the choice and set is_theme_preset to preset
+            let preset = Presets::try_from(theme).unwrap();
+
+            let user_update_query = sqlx::query("UPDATE users SET theme_preset=t.preset, is_theme_preset=TRUE FROM themes AS t WHERE uuid=($1), t.theme = ($2);")
+                .bind(user_user.get_uuid())
+                .bind(preset)
+                .execute(pool)
+                .await;
+
+            if let Err(err) = user_update_query {
+                return Err(err);
+            } else {
+                return Ok(());
+            }
+        },
     }
 }
