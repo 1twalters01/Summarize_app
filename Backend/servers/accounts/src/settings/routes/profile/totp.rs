90400123587de5e136492ad0cab905d231775167
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2024-08-19 21:38:43 +0100

    Update totp.rs

--- Backend/servers/accounts/src/settings/routes/profile/totp.rs
+++ Backend/servers/accounts/src/settings/routes/profile/totp.rs
@@ -13,7 +13,9 @@ use crate::{
         },
     },
     utils::{
-        database_connections::{create_pg_pool_connection, create_redis_client_connection, set_key_value_in_redis}, tokens::generate_opaque_token_of_length, validations::{validate_password, validate_totp}
+        database_connections::{create_pg_pool_connection, create_redis_client_connection, set_key_value_in_redis},
+        tokens::generate_opaque_token_of_length,
+        validations::{validate_password, validate_totp}
     },
 };
 use actix_protobuf::{ProtoBuf, ProtoBufResponseBuilder};
@@ -225,7 +227,7 @@ pub async fn post_confirmation(
             
     // get current totp status
     let pool = create_pg_pool_connection().await;
-    let totp_status: bool = match get_totp_status_from_uuid_in_pg_users_table(&pool, &user_uuid).await {
+    let totp_key: Option<String> = match get_totp_key_from_uuid_in_pg_users_table(&pool, &user_uuid).await {
         Ok(result) => result,
         Err(_) => {
             let response: TotpResponse = TotpResponse {
@@ -240,21 +242,7 @@ pub async fn post_confirmation(
     };
     
     // if totp = there then delete
-    if totp_status == true {
-        let totp_key: String = match get_totp_key_from_uuid_in_pg_users_table(&pool, &user_uuid).await {
-            Ok(result) => result,
-            Err(_) => {
-                let response: TotpResponse = TotpResponse {
-                    response_field: Some(totp_response::ResponseField::Error(
-                        TotpError::ServerError as i32,
-                    )),
-                };
-                return Ok(HttpResponse::InternalServerError()
-                    .content_type("application/x-protobuf; charset=utf-8")
-                    .protobuf(response));
-            },
-        };
-
+    if let Some(totp_key) = totp_key {
         let mut totp: Totp = Totp::from_key(totp_key);
         
         if totp.verify(digit1, digit2, digit3, digit4, digit5, digit6).is_err() {
@@ -318,25 +306,26 @@ pub async fn post_confirmation(
 }
 
 use sqlx::{Pool, Postgres};
-async fn get_totp_status_from_uuid_in_pg_users_table(
-    pool: &Pool<Postgres>,
-    user_uuid: &str,
-) -> Result<bool, sqlx::Error> {
-    let user_select_query = sqlx::query("").bind(user_uuid).execute(pool).await;
-
-    if let Err(err) = user_select_query {
-        return Err(err);
-    } else {
-        let result: bool = true;
-        return Ok(result);
-    }
-}
-
 async fn get_totp_key_from_uuid_in_pg_users_table(
     pool: &Pool<Postgres>,
     user_uuid: &str,
-) -> Result<String, sqlx::Error> {
-    let user_select_query = sqlx::query("").bind(user_uuid).execute(pool).await;
+) -> Result<Option<String>, sqlx::Error> {
+    let user_select_query = sqlx::query("Select totp_key from users WHERE uuid=($1")
+        .bind(user_uuid)
+        .execute(pool)
+        .await;
+    
+    match user_select_query {
+        Err(err) => return Err(err),
+        Ok(res) => {
+            if res.len() == 0 {
+                return Ok(None);
+            }
+
+            let key: Option<String> = res[0].get("totp_key");
+            return Ok(key);
+        }
+    }
 
     if let Err(err) = user_select_query {
         return Err(err);
@@ -350,7 +339,7 @@ async fn delete_totp_from_uuid_in_pg_users_table(
     pool: &Pool<Postgres>,
     user_uuid: &str,
 ) -> Result<(), sqlx::Error> {
-    let user_select_query = sqlx::query("").bind(user_uuid).execute(pool).await;
+    let user_select_query = sqlx::query("UPDATE users SET totp_key=NULL WHERE uuid=($1)").bind(user_uuid).execute(pool).await;
 
     if let Err(err) = user_select_query {
         return Err(err);
@@ -364,13 +353,13 @@ async fn set_totp_for_uuid_in_pg_users_table(
     user_uuid: &str,
     totp: &Totp,
 ) -> Result<(), sqlx::Error>{
-    let user_select_query = sqlx::query("")
-        .bind(user_uuid)
+    let user_update_query = sqlx::query("UPDATE users SET totp_key=($1) WHERE uuid=($2)")
         .bind(totp.get_url())
+        .bind(user_uuid)
         .execute(pool)
         .await;
 
-    if let Err(err) = user_select_query {
+    if let Err(err) = user_update_query {
         return Err(err);
     } else {
         return Ok(());
