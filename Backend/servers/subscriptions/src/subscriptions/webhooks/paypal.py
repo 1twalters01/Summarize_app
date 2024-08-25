97b3b7c7b51af1bf4daa2f80323f20714522e518
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2024-08-25 11:13:47 +0100

    Update paypal.py

--- Backend/servers/subscriptions/src/subscriptions/webhooks/paypal.py
+++ Backend/servers/subscriptions/src/subscriptions/webhooks/paypal.py
@@ -19,13 +19,12 @@ class PaypalEvent(BaseModel):
 async def paypal_webhooks(event: PaypalEvent):
     if event.event_type == "BILLING.SUBSCRIPTION.ACTIVATED":
         print("Billing.subscription.activated") # log event
-        subscription_id = event.resource.id
+        subscription_id = event.resource.id # Encrypt from here?
         user_uuid = get_subscriber_or_none(subscription_id, 'Paypal')
         if user_uuid == None:
             response = {"success": False}
             return JSONResponse(content=response, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
-        # Add the subscription id to the database
         # get current end date
         end_date = get_end_date(user_uuid)
         if subscriber.trial == False:
@@ -33,7 +32,7 @@ async def paypal_webhooks(event: PaypalEvent):
         else:
             end_date = max(end_date, datetime.now() + relativedelta(months=1, weeks=2, days=2))
     
-        subscription_id = encrypt(subscription_id)
+        subscription_id = encrypt(subscription_id) # Move up?
         start_date = datetime.now()
         # set subscription_id=subscription_id, start_date=start_date, trial_status=False, end_date=end_date, subscribed=true WHERE uuid=user_uuid
         response = {"success": True}
@@ -42,24 +41,22 @@ async def paypal_webhooks(event: PaypalEvent):
     # Whenever I get paid
     elif event.event_type == "PAYMENT.SALE.COMPLETED":
         print("PAYMENT.SALE.COMPLETED") # log event
-        subscription_id = event.resource.id
+        subscription_id = event.resource.id # Encrypt here?
         user_uuid = get_subscriber_or_none(subscription_id, 'Paypal')
-    
         if user_uuid == None:
             response = {"success": False}
             return JSONResponse(content=response, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
-        user = req_userprofile.user
-        subscriber = UserProfile.objects.get(user=user)
-        subscriber.subscription_id = encrypt(subscription_id)
-        subscriber.subscribed = True
-        subscriber.start_date = date.today()
+        # get current end date
+        end_date = get_end_date(user_uuid)
         if subscriber.trial == False:
-            subscriber.end_date = date.today() + relativedelta(months=1, days=2)
+            end_date = max(end_date, datetime.now() + relativedelta(months=1, days=2))
         else:
-            subscriber.end_date = date.today() + relativedelta(months=1, weeks=2, days=2)
-        subscriber.trial = False
-        subscriber.save()
+            end_date = max(end_date, datetime.now() + relativedelta(months=1, weeks=2, days=2))
+            
+        subscriber.subscription_id = encrypt(subscription_id) # Move encryption up?
+        subscriber.start_date = date.today()
+        # set subscription_id=subscription_id, start_date=start_date, trial_status=False, end_date=end_date, subscribed=true WHERE uuid=user_uuid
         response = {"success": True}
         return JsonResponse(response, status=200)
 
@@ -68,8 +65,7 @@ async def paypal_webhooks(event: PaypalEvent):
     elif event.event_type == "Billing.subscription.expired":
         print("Billing.subscription.expired") # log event
         subscription_id = event.resource.id
-        req_userprofile = get_subscriber_or_none(subscription_id, 'Paypal')
-
+        user_uuid = get_subscriber_or_none(subscription_id, 'Paypal')
         if user_uuid == None:
             response = {"success": False}
             return JSONResponse(content=response, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
