b8203dbe5beaf0762cfe2f74366b7ee0a21b9c94
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2024-08-27 11:03:42 +0100

    Update urls.py

--- Backend/servers/subscriptions/src/subscriptions/urls.py
+++ Backend/servers/subscriptions/src/subscriptions/urls.py
@@ -3,6 +3,7 @@ from .webhooks.stripe import stripe_webhook
 from .webhooks.paypal import paypal_webhook
 from .webhooks.coinbase import coinbase_webhook
 # from .webhooks import stripe.stripe_webhook, paypal.paypal_webhook, coinbase.coinbase_webhook
+# from .views.status import RetrieveStatus
 from .views.stripe import NewStripeCustomer
 from .views.paypal import NewPaypalCustomer
 from .views.coinbase import NewCoinbaseCustomer
@@ -13,6 +14,7 @@ router = APIRouter()
 router.add_api_route("/webhooks/stripe", stripe_webhook, methods=["POST"])
 router.add_api_route("/webhooks/paypal", paypal_webhook, methods=["POST"])
 # router.add_api_route("/webhooks/coinbase", coinbase_webhook, methods=["POST"])
-router.add_api_route("/subscribe/stripe", stripe_webhook, methods=["POST"])
-router.add_api_route("/subscribe/paypal", paypal_webhook, methods=["POST"])
-# router.add_api_route("/subscriptions/coinbase", coinbase_webhook, methods=["POST"])
+router.add_api_route("/subscribe/status", RetrieveStatus, methods=["POST"])
+router.add_api_route("/subscribe/stripe", NewStripeCustomer, methods=["POST"])
+router.add_api_route("/subscribe/paypal", NewPaypalCustomer, methods=["POST"])
+# router.add_api_route("/subscriptions/coinbase", NewCoinbaseCustomer, methods=["POST"])
