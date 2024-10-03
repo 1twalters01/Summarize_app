88093fcd6256f8f74032cf09f564c2fef796d58b
1twalters01 <1twalters01@gmail.com>
2024-10-03 20:03:41 +0100

    basics

--- Backend/servers/14 linux/src/main.rs
+++ Backend/servers/14 linux/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web::{get, route, Bytes}, App, HttpResponse, HttpServer};
+use actix_files as files;
+use std::{env, fs, path::PathBuf};
+use dotenv::dotenv;
+mod utils;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    HttpServer::new(|| {
+        let website_dir: String = env::var("WEBSITE_DIR").unwrap();
+
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
