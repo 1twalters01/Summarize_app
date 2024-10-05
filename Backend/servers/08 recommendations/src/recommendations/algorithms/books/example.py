1b7ac9bb49c324d9e74dc7b75206b0a4f3792744
1twalters01 <1twalters01@gmail.com>
2024-10-05 15:19:52 +0100

    recommendation example

--- Backend/servers/08 recommendations/src/recommendations/algorithms/books/example.py
+++ Backend/servers/08 recommendations/src/recommendations/algorithms/books/example.py
@@ -2,31 +2,31 @@ import json
 
 books_db = [
     {
-        "id": "aaaa",
+        "id": "e09b06aa-0dfa-4d21-ae79-edc0cd10a6e6",
         "title": "The Art of Electronics",
         "authors": ["Paul Horowitz", "Winfield Hill"],
         "genres": ["Electrical Engineering", "Engineering", "Non-fiction"],
     },
     {
-        "id": "bbbb",
+        "id": "857db374-123c-435b-9901-389ac9eaf0b7",
         "title": "The Laws of Human Nature",
         "authors": ["Robbert Greene"],
         "genres": ["Self help", "Psychology", "Non-fiction"],
     },
     {
-        "id": "cccc",
+        "id": "2a9089f2-01c9-4f97-8f3e-69e3a5fcd04d",
         "title": "TCP/IP Guide",
         "authors": ["Charles M. Kozierok"],
         "genres": ["Software Engineering", "Engineering", "Non-fiction"],
     },
     {
-        "id": "dddd",
+        "id": "dc924fb5-38c2-43ae-a7ec-540dc51c14e9",
         "title": "Design Patterns Elements of Reusable Object-Oriented Software",
         "authors": ["Erich Gamma", "John Vlissides", "Richard Helm", "Ralph Johnson"],
         "genres": ["Software Architecture", "Software Engineering", "Engineering", "Non-fiction"],
     },
     {
-        "id": "eeee",
+        "id": "737b74b4-858f-4ac5-a615-46158fa46495",
         "title": "The Algorithm Design Manual",
         "authors": ["Steven S. Skiena"],
         "genres": ["Software Architecture", "Algorithms", "Software Engineering", "Engineering", "Non-fiction"],
@@ -45,7 +45,6 @@ def get_recommendations(book_id, genre_level=10, recommendation_number=15):
         recommendations += [
             book for book in books_db if genre in book["genres"] and book["id"] != book_id and book not in recommendations
         ]
-    # replace json.dumps for a rust thing for speed?
     return json.dumps(recommendations[:recommendation_number], indent=4)
 
 if __name__ == "__main__":
--- Backend/servers/08 recommendations/src/recommendations/views/books/example.rs
+++ Backend/servers/08 recommendations/src/recommendations/views/books/example.rs
@@ -1,72 +1,71 @@
-use std::collections::HashMap;
-
 use actix_protobuf::{ProtoBuf, ProtoBufResponseBuilder};
 use actix_web::{HttpResponse, Responder, Result};
-use pyo3::{prelude::*, types::{PyDict, PyTuple}, Py, PyAny};
+use pyo3::prelude::*;
 use serde::Deserialize;
 use crate::{
     generated::protos::recommendations::books::example::{
         request::Request,
-        response::{response::ResponseField, Error, Response, Success},
+        response::{response::ResponseField, Error, Response, Success, BookRecommendation},
     },
     utils::validations::{validate_book_id, validate_genre_level, validate_recommendation_number}
 };
 
-#[derive(Debug)]
-struct BookRecommendations {
-    id: String,
-    title: String,
-    authors: Vec<String>,
-    genres: Vec<String>
+#[derive(Debug, Deserialize)]
+pub struct Recommendation {
+    pub id: String,
+    pub title: String,
+    pub authors: Vec<String>,
+    pub genres: Vec<String>
 }
 
 pub async fn post_book_id(data: ProtoBuf<Request>) -> Result<impl Responder> {
-    // get request variable
+    // get request variables
     let Request { book_id, genre_level, recommendation_number } = data.0;
 
     // Validate variables from the request body
-    // let validated_book_id = validate_book_id(&book_id);
-    // if validated_book_id.is_err() {
-    //     let response: Response = Response {
-    //         response_field: Some(ResponseField::Error(Error::InvalidBookId as i32)),
-    //     };
-    //
-    //     return Ok(HttpResponse::UnprocessableEntity()
-    //         .content_type("application/x-protobuf; charset=utf-8")
-    //         .protobuf(response));
-    // }
-    // let validated_genre_level = validate_genre_level(genre_level);
-    // if validated_genre_level.is_err() {
-    //     let response: Response = Response {
-    //         response_field: Some(ResponseField::Error(Error::InvalidGenreLevel as i32)),
-    //     };
-    //
-    //     return Ok(HttpResponse::UnprocessableEntity()
-    //         .content_type("application/x-protobuf; charset=utf-8")
-    //         .protobuf(response));
-    // }
-    // let validated_recommendation_number = validate_recommendation_number(recommendation_number);
-    // if validated_recommendation_number.is_err() {
-    //     let response: Response = Response {
-    //         response_field: Some(ResponseField::Error(Error::InvalidRecommendationNumber as i32)),
-    //     };
-    //
-    //     return Ok(HttpResponse::UnprocessableEntity()
-    //         .content_type("application/x-protobuf; charset=utf-8")
-    //         .protobuf(response));
-    // }
+    let validated_book_id = validate_book_id(&book_id);
+    if validated_book_id.is_err() {
+        let response: Response = Response {
+            response_field: Some(ResponseField::Error(Error::InvalidBookId as i32)),
+        };
 
-    let recommendations: Result<Vec<BookRecommendations>, ()> = get_recommendations(book_id, genre_level, recommendation_number);
+        return Ok(HttpResponse::UnprocessableEntity()
+            .content_type("application/x-protobuf; charset=utf-8")
+            .protobuf(response));
+    }
+
+    let validated_genre_level = validate_genre_level(genre_level);
+    if validated_genre_level.is_err() {
+        let response: Response = Response {
+            response_field: Some(ResponseField::Error(Error::InvalidGenreLevel as i32)),
+        };
+        return Ok(HttpResponse::UnprocessableEntity()
+            .content_type("application/x-protobuf; charset=utf-8")
+            .protobuf(response));
+    }
+
+    let validated_recommendation_number = validate_recommendation_number(recommendation_number);
+    if validated_recommendation_number.is_err() {
+        let response: Response = Response {
+            response_field: Some(ResponseField::Error(Error::InvalidRecommendationNumber as i32)),
+        };
+        return Ok(HttpResponse::UnprocessableEntity()
+            .content_type("application/x-protobuf; charset=utf-8")
+            .protobuf(response));
+    }
+
+    let recommendations: Result<Vec<BookRecommendation>, ()> = get_recommendations(book_id, genre_level, recommendation_number);
     match recommendations {
         Ok(recommendations) => {
             let response: Response = Response {
-                response_field: Some(ResponseField::Success(Success {}))
+                response_field: Some(ResponseField::Success(Success {recommendations}))
             };
             return Ok(HttpResponse::Ok()
                 .content_type("application/x-protobuf; charset=utf-8")
                 .protobuf(response));
         },
         Err(err) => {
+            println!("err: {:#?}", err);
             let response: Response = Response {
             response_field: Some(ResponseField::Error(Error::ServerError as i32)),
             };
@@ -77,49 +76,15 @@ pub async fn post_book_id(data: ProtoBuf<Request>) -> Result<impl Responder> {
     }
 }
 
-pub fn get_recommendations(book_id: String, genre_level: i32, recommendation_number: i32) -> Result<Vec<BookRecommendations>, ()> {
-    let arg1 = "arg1";
-    let arg2 = "arg2";
-    let arg3 = "arg3";
-
+pub fn get_recommendations(book_id: String, genre_level: i32, recommendation_number: i32) -> Result<Vec<BookRecommendation>, ()> {
     pyo3::prepare_freethreaded_python();
     
-    // Python::with_gil(|py| {
-    //     let fun: Py<PyAny> = PyModule::from_code_bound(
-    //         py,
-    //         "def example(*args, **kwargs):
-    //             if args != ():
-    //                 print('called with args', args)
-    //             if kwargs != {}:
-    //                 print('called with kwargs', kwargs)
-    //             if args == () and kwargs == {}:
-    //                 print('called with no arguments')",
-    //         "",
-    //         "",
-    //     ).unwrap()
-    //     .getattr("example").unwrap()
-    //     .into();
-    //     println!("{:#?}", fun);
-    //
-    //     // call object without any arguments
-    //     fun.call0(py).unwrap();
-    //
-    //     // pass object with Rust tuple of positional arguments
-    //     let args = (arg1, arg2, arg3);
-    //     fun.call1(py, args).unwrap();
-    //
-    //     // call object with Python tuple of positional arguments
-    //     let args = PyTuple::new_bound(py, &[arg1, arg2, arg3]);
-    //     fun.call1(py, args).unwrap();
-    // });
-
     let py_code = include_str!(concat!(
             env!("CARGO_MANIFEST_DIR"),
             "/src/recommendations/algorithms/books/example.py"
             ));
-    // println!("{:?}", py_code);
 
-    Python::with_gil(|py| {
+    return Python::with_gil(|py| {
         let py_engine = PyModule::from_code_bound(py, py_code, "recommendations.algorithms.books.example", "recommendations.algorithms.books.example")
             .map_err(|err| format!("Failed to import Python module: {:?}", err))
             .unwrap();
@@ -133,62 +98,18 @@ pub fn get_recommendations(book_id: String, genre_level: i32, recommendation_num
             .call1((book_id, genre_level, recommendation_number))
             .map_err(|err| format!("Python function call failed: {:?}", err))
             .unwrap().to_string();
-        // println!("py_recommendations: {:#?}", py_recommendations);
 
-        #[derive(Debug, Deserialize)]
-        struct Book {
-            id: String,
-            title: String,
-            authors: Vec<String>,
-            genres: Vec<String>,
-        }
-
-        // use extract instead of serde for speed?
-        let books: Vec<Book> = serde_json::from_str(&py_recommendations).unwrap();
-        println!("Books: {:#?}", books);
-        // let recommendations = py_recommendations
-        //     .extract::<Vec<HashMap<String, Vec<String> >>>()
-        //     .unwrap();
-        // println!("recommendations: {:#?}", recommendations);
-
-    //     let recommendations = py_recommendations
-    //         .extract::<Vec<PyObject>>()
-    //         .map_err(|err| format!("Failed to extract Python object: {:?}", err))?;
-    //
-    //     let recommendations: Vec<BookRecommendations> = recommendations
-    //         .into_iter()
-    //         .map(|py_obj| {
-    //             let py_any: PyDict = py_obj.as_any().into()?;
-    //             
-    //             Ok(BookRecommendations {
-    //                 id: py_dict.get_item("id").unwrap().extract::<String>()?,
-    //                 title: py_dict.get_item("title").unwrap().extract::<String>()?,
-    //                 authors: py_dict.get_item("authors").unwrap().extract::<Vec<String>>()?,
-    //                 genres: py_dict.get_item("genres").unwrap().extract::<Vec<String>>()?,
-    //             })
-    //         })
-    //         .collect::<Result<Vec<BookRecommendations>, _>>()?;
-    //     return Ok(recommendations)
+        let books: Vec<Recommendation> = serde_json::from_str(&py_recommendations).unwrap();
+        let recommendations = books.into_iter().map(|recommendation| BookRecommendation {id: recommendation.id, title: recommendation.title, authors: recommendation.authors, genres: recommendation.genres}).collect::<Vec<BookRecommendation>>();
+        Ok::<Vec<BookRecommendation>, ()>(recommendations)
     });
-    // return Err(());
-
-    let book_recommendations: Vec<BookRecommendations> = vec![
-        BookRecommendations {
-            id: "aaaa".to_string(),
-            title: "The Art of Electronics".to_string(),
-            authors: vec!["Paul Horowitz".to_string(), "Winfield Hill".to_string()],
-            genres: vec!["Electrical Engineering".to_string(), "Engineering".to_string(), "Non-fiction".to_string()],
-        }
-    ];
-    // println!("{:#?}", book_recommendations);
-    return Ok(book_recommendations)
 }
 
 #[cfg(test)]
 mod tests {
     use actix_web::{test, web, App};
     use bytes::Bytes;
-    use prost::Message;
+    use prost::{DecodeError, Message};
 
     use crate::generated::protos::recommendations::books::example::{request::Request, response::Response};
 
@@ -204,7 +125,7 @@ mod tests {
         )
         .await;
 
-        let book_id = "cccc".to_string();
+        let book_id = "2a9089f2-01c9-4f97-8f3e-69e3a5fcd04d".to_string();
         let genre_level = 1;
         let recommendation_number = 5;
         let req_message = Request { book_id, genre_level, recommendation_number };
@@ -220,8 +141,14 @@ mod tests {
 
         let resp = test::call_service(&mut app, request).await;
         let response_buffer: Vec<u8> = test::read_body(resp).await.to_vec();
-        let decoded: Response = Message::decode(&response_buffer[..]).unwrap();
+        let decoded_result: Result<Response, DecodeError> = Message::decode(&response_buffer[..]);
 
-        panic!("boo")
+        match decoded_result {
+            Ok(_) => assert!(true),
+            Err(err) => {
+                println!("err: {:#?}", err);
+                panic!("");
+            },
+        }
     }
 }
