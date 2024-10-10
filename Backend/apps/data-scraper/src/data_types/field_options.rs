fbba1c8a2b4d3bc814138854cf3efa157d4f0515
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2024-10-10 07:18:05 +0100

    initial main book data structure

--- Backend/apps/data-scraper/src/data_types/field_options.rs
+++ Backend/apps/data-scraper/src/data_types/field_options.rs
@@ -1,10 +1,9 @@
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-#[derive(Serialize, Deserialize)]
-pub struct Genre {
-    pub genre: String,
-    pub root: Option<Genre>,
+pub struct Publisher {
+    pub publisher: String,
+    pub root: Publisher,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -18,11 +17,29 @@ pub struct Author {
 
 #[derive(Serialize, Deserialize)]
 pub struct Book {
-    pub title: String,
-    pub subtitle: String,
+    pub title: Vec<String>,
+    pub languages: Vec<String>,
+    pub subtitle: Vec<String>,
+    pub isbn: i64,
+    pub isbn_13: i64,
+    pub authors: Vec<Author>,
+    pub co_authors: Vec<Author>,
+    pub publisher: Publisher,
+    pub edition: String,
+    pub publish_date: SystemTime,
+    pub binding: String,
+    pub pages: i64,
+    pub synopsis: String,
+    pub dimensions: Dimensions, // height, length, width
+    pub weight: Weight,
+    pub genres: Vec<Genre>,
+    pub ratings: Ratings,
+    pub links: Links, 
 }
 
-pub struct Publisher {
-    pub publisher: String,
-    pub root: Publisher,
+#[derive(Serialize, Deserialize)]
+pub struct Genre {
+    pub genre: String,
+    pub supergenre: Option<Genre>,
+    pub subgenres: Vec<Option<Genre>>,
 }
