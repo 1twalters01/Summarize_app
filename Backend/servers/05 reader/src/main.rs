39e7b519ab6798f1b1c796fe2d9ed4bc45b0a728
1twalters01 <1twalters01@gmail.com>
2024-10-22 17:58:50 +0100

    shuffle server folder numbers

--- Backend/servers/05 reader/src/main.rs
+++ Backend/servers/05 reader/src/main.rs
@@ -2,20 +2,19 @@ use actix_cors::Cors;
 use actix_web::{App, HttpServer};
 use dotenv::dotenv;
 
+pub mod reader;
 pub mod generated;
 pub mod middleware;
 pub mod ping;
-pub mod recommendations;
-// pub mod settings;
+pub mod settings;
 pub mod utils;
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() {
     dotenv().ok();
-
+    
     HttpServer::new(|| {
         let cors = Cors::default()
-            .allowed_origin("http://127.0.0.1:8009")
+            .allowed_origin("http://127.0.0.1:8006")
             .allow_any_header()
             .allow_any_method()
             .expose_any_header();
@@ -23,10 +22,10 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(cors)
             .configure(ping::urls::config)
-            // .configure(settings::urls::config)
-            .configure(recommendations::urls::config)
+            .configure(reader::urls::config)
+            .configure(settings::urls::config)
     })
-    .bind("127.0.0.1:8006")?
+    .bind("127.0.0.1:8005")?
     .run()
     .await
 }
--- Backend/servers/10 recommendations/src/main.rs
+++ Backend/servers/10 recommendations/src/main.rs
@@ -2,19 +2,20 @@ use actix_cors::Cors;
 use actix_web::{App, HttpServer};
 use dotenv::dotenv;
 
-pub mod reader;
 pub mod generated;
 pub mod middleware;
 pub mod ping;
-pub mod settings;
+pub mod recommendations;
+// pub mod settings;
 pub mod utils;
 
-async fn main() {
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
     dotenv().ok();
-    
+
     HttpServer::new(|| {
         let cors = Cors::default()
-            .allowed_origin("http://127.0.0.1:8006")
+            .allowed_origin("http://127.0.0.1:8010")
             .allow_any_header()
             .allow_any_method()
             .expose_any_header();
@@ -22,8 +23,8 @@ async fn main() {
         App::new()
             .wrap(cors)
             .configure(ping::urls::config)
-            .configure(reader::urls::config)
-            .configure(settings::urls::config)
+            // .configure(settings::urls::config)
+            .configure(recommendations::urls::config)
     })
     .bind("127.0.0.1:8006")?
     .run()
