d7639fe829835380ea1adac5b7c06db66f36e0db
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2024-12-05 22:06:49 +0000

    Add authentication dependency to subscriptions

--- Backend/servers/02 subscriptions/src/routes/settings.py
+++ Backend/servers/02 subscriptions/src/routes/settings.py
@@ -1,8 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from src.middleware.authentication import is_authenticated
 
 from src.views.settings.premium import get_premium_status
 
 router = APIRouter()
 
-router.add_api_route("/settings/subscription", get_premium_status, methods=["POST"])
+router.add_api_route(
+    "/settings/subscription",
+    get_premium_status,
+    methods=["POST"],
+    dependencies = [Depends(is_authenticated)]
+)
 
--- Backend/servers/02 subscriptions/src/routes/subscriptions.py
+++ Backend/servers/02 subscriptions/src/routes/subscriptions.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from src.middleware.authentication import is_authenticated
 from src.views.subscriptions.status import retrieve_status
 from src.views.subscriptions.stripe import create_stripe_customer
 from src.views.subscriptions.paypal import create_paypal_customer
@@ -6,6 +7,21 @@ from src.views.subscriptions.paypal import create_paypal_customer
 
 router = APIRouter()
 
-router.add_api_route("/subscribe/status", retrieve_status, methods=["POST"])
-router.add_api_route("/subscribe/stripe", create_stripe_customer, methods=["POST"])
-router.add_api_route("/subscribe/paypal", create_paypal_customer, methods=["POST"])
+router.add_api_route(
+    "/subscribe/status",
+    retrieve_status,
+    methods=["POST"],
+    dependencies = [Depends(is_authenticated)]
+)
+router.add_api_route(
+    "/subscribe/stripe",
+    create_stripe_customer,
+    methods=["POST"],
+    dependencies = [Depends(is_authenticated)]
+)
+router.add_api_route(
+    "/subscribe/paypal",
+    create_paypal_customer,
+    methods=["POST"],
+    dependencies = [Depends(is_authenticated)]
+)
