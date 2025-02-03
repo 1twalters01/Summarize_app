fc5f92f75728a0092a7686fb6073668a2083797b
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2025-02-03 10:15:51 +0000

    initialise views

--- Backend/servers/02 subscriptions/src/routes/subscriptions.py
+++ Backend/servers/02 subscriptions/src/routes/subscriptions.py
@@ -3,8 +3,7 @@ from src.middleware.authentication import is_authenticated
 from src.views.subscriptions.status import retrieve_status_view
 from src.views.subscriptions.plans import retrieve_plans_view
 from src.views.subscriptions.cancellation import cancel_subscription_view
-from src.views.subscriptions.create_stripe import create_stripe_subscription_view
-from src.views.subscriptions.create_paypal import create_paypal_subscription_view
+from src.views.subscriptions.create import create_subscription_view
 
 router.add_api_route(
     "/subscription/plans",
