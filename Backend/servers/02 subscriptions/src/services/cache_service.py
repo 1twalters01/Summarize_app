275da9d292b8825dbf95dd567ed6b33f2a194a30
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2025-02-28 11:05:45 +0000

    apply discounts view

--- Backend/servers/02 subscriptions/src/services/cache_service.py
+++ Backend/servers/02 subscriptions/src/services/cache_service.py
@@ -6,17 +6,22 @@ class CacheService():
     def __init__(self, client: Redis):
         self.client = client
 
-    def store_value_for_key(value: str, key: str, ex: int|None):
-        if ex == None:
-            r.set(key, value)
-        elif type(ex) == int:
-            r.set(key, ex)
+    def store_value_for_key(self, value: str, key: str, expiration_in_seconds: int|None):
+        if expiration_in_seconds == None:
+            self.client.set(key, value)
+        elif type(expiration_in_seconds) == int:
+            self.client.set(key, expiration_in_seconds)
         else:
             return TypeError
 
-    def get_value_from_key(key: str):
-        value = r.get(key)
+    def get_value_from_key(self, key: str):
+        value = self.client.get(key)
         return value
 
-    def delete_key(key: str):
-        r.delete(key)
+    def delete_key(self, key: str):
+        self.client.delete(key)
+
+    def store_discount_code_for_user_uuid(self, discount_code: str, user_uuid: str, expiration_in_seconds: int|None):
+        key = f"discount for {user_uuid}"
+        value = discount_code
+        self.store_value_for_key(value, key, expiration_in_seconds)
--- Backend/servers/02 subscriptions/src/views/billing/discounts.py
+++ Backend/servers/02 subscriptions/src/views/billing/discounts.py
@@ -3,6 +3,8 @@ from datetime import datetime
 from src.datatypes.payment_type import PaymentTypeEnum
 from src.datatypes.discount_class import DiscountClass
 from src.datatypes.payment_tier import PaymentTierEnum
+from src.services.cache_service import CacheService
+from src.utils.database_connections import get_redis_client_connection
 
 async def apply_discount_view(request: Request, discount_code: str, payment_type: PaymentTypeEnum, tier: PaymentTierEnum):
     discount: DiscountClass = discount_service.get_discount_from_code(discount_code)
@@ -20,7 +22,8 @@ async def apply_discount_view(request: Request, discount_code: str, payment_type
         raise HTTPException(status_code=400, detail="Discount code is not valid for the given payment type")
 
     user_uuid = request.state.user_uuid
-    # cache key: f"discount for {user_uuid}", value: code
+    cache = CacheService(get_redis_client_connection())
+    cache.store_discount_code_for_user_uuid(discount_code, user_uuid, expiration_in_seconds)
 
     return {
         "discount_value": discount.discount_value,
